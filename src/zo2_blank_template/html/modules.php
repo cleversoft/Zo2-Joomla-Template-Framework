8ca2e8dc38867e2862dd0da4931822d4e20a4a14
Quynh <nvanquynh89>
2013-09-30 08:36:50 +0700

    update templateDetails.xml and folder html

--- src/zo2_blank_template/html/modules.php
+++ src/zo2_blank_template/html/modules.php
@@ -13,3 +13,68 @@
  */
 //no direct accees
 defined('_JEXEC') or die ('resticted aceess');
+
+$modChromes = array('zo2_xhtml', 'zo2_flat', 'zo2_raw', 'zo2_menu', 'none'  );
+
+function modChrome_zo2_xhtml($module, $params, $attribs)
+{ ?>
+    <div class="module <?php echo $params->get('moduleclass_sfx'); ?>">
+        <div class="mod-wrapper clearfix">
+            <?php if ($module->showtitle != 0) { ?>
+                <h3 class="moduletitle">
+                    <?php
+                    echo '<span>'.$module->title.'</span>';
+                    ?>
+                </h3>
+                <?php
+                $modsfx=$params->get('moduleclass_sfx');
+                if ($modsfx !='') echo '<span class="sp-badge ' . $modsfx . '"></span>';
+                ?>
+            <?php } ?>
+            <div class="mod-content clearfix">
+                <div class="mod-inner clearfix">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="gap"></div>
+<?php
+}
+
+function modChrome_zo2_flat($module, $params, $attribs)
+{ ?>
+    <div class="module <?php echo $params->get('moduleclass_sfx'); ?>">
+        <div class="mod-wrapper-flat clearfix">
+            <?php if ($module->showtitle != 0) { ?>
+                <h3 class="moduletitle">
+                    <?php
+                    echo '<span>'.$module->title.'</span>';
+                    ?>
+                </h3>
+                <?php
+                $modsfx=$params->get('moduleclass_sfx');
+                if ($modsfx !='') echo '<span class="sp-badge ' . $modsfx . '"></span>';
+                ?>
+            <?php } ?>
+            <?php echo $module->content; ?>
+        </div>
+    </div>
+    <div class="gap"></div>
+<?php
+}
+
+function modChrome_zo2_raw($module, $params, $attribs)
+{
+    echo $module->content;
+}
+
+function modChrome_zo2_menu($module, $params, $attribs)
+{ ?>
+    <div class="module <?php echo $params->get('moduleclass_sfx'); ?>">
+        <div class="mod-wrapper-menu clearfix">
+            <?php echo $module->content; ?>
+        </div>
+    </div>
+<?php
+}
