a0359a2b4a8d65bc77b9ed9a5153efba16a934f1
Quynh <nvanquynh89>
2013-09-30 15:21:47 +0700

    Update file edit_layoutbuilder.php

--- src/zo2/html/admin/com_templates/style/edit_layoutbuilder.php
+++ src/zo2/html/admin/com_templates/style/edit_layoutbuilder.php
@@ -15,13 +15,8 @@ defined('_JEXEC') or die;
 ?>
 
 <?php foreach ($this->form->getFieldset('layout') as $field) : ?>
-    <div class="control-group">
-        <div class="control-label">
-            <?php echo $field->label; ?>
-        </div>
-        <div class="controls">
+
             <?php echo $field->input; ?>
-        </div>
-    </div>
+     
 <?php endforeach;
 
