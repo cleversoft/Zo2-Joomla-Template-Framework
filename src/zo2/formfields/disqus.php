a85edd6a7b68b26017cbda8a38c6a2f2dc7ff39b
Vu Quoc Hiep <vqhiep2010@gmail.com>
2013-10-01 16:53:56 +0700

    Added the description for each input field

---
+++ src/zo2/formfields/disqus.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Zo2 (http://www.zo2framework.org)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zo2framework.org
+ * @link        http://github.com/aploss/zo2
+ * @author      Duc Nguyen <ducntv@gmail.com>
+ * @author      Hiepvu <vqhiep2010@gmail.com>
+ * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die;
+
+class JFormFieldDisqus extends JFormFieldText
+{
+    protected $type = 'Disqus';
+
+    /**
+     * Get the html for input
+     *
+     * @return string
+     */
+    public function getInput()
+    {
+        $html = '<div style="margin: 5px 0;">
+                    <strong>Notes </strong>: Required if showing the Disqus Tab
+                </div>';
+
+        return parent::getInput() . $html ;
+    }
+}
---
+++ src/zo2/formfields/taborder.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Zo2 (http://www.zo2framework.org)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zo2framework.org
+ * @link        http://github.com/aploss/zo2
+ * @author      Duc Nguyen <ducntv@gmail.com>
+ * @author      Hiepvu <vqhiep2010@gmail.com>
+ * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die;
+
+class JFormFieldTaborder extends JFormFieldText
+{
+    protected $type = 'Taborder';
+
+    /**
+     * Get the html for input
+     *
+     * @return string
+     */
+    public function getInput()
+    {
+        $html = '<div style="margin: 5px 0;">
+                    <strong>Notes</strong>: Comma Separated List, First listed is the default, If left empty it will use default value below, only tabs listed will be shown. <br />
+                    <strong>Possible Values </strong>: gplus,facebook,disqus,k2comment <br />
+                    <strong>Default Value </strong>: gplus,facebook,k2comment <br />
+                </div>';
+
+        return parent::getInput() . $html ;
+    }
+}
