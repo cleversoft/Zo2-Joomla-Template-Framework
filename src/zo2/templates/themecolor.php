b5a224d98d3c9ba303d9ba9bbc64d05fcc844a30
Duc Nguyen <ducntq@gmail.com>
2013-10-30 14:00:37 +0700

    #28: Updated plugin's files with new header

--- src/zo2/templates/themecolor.php
+++ src/zo2/templates/themecolor.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Zo2 (http://www.zo2framework.org)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zo2framework.org
+ * @link        http://github.com/aploss/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
+ * @license     GPL v2
+ */
 $templatePath = JUri::root(true) . '/templates/' . $zo2->getTemplateName();
 if (!isset($presets)) $presets = array();
 if (!empty($this->value)) $currentData = json_decode($this->value, true);
