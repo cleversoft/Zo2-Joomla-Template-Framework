707bc80b9d4e37d7d9b93a0eff9d91d3dbd47b2e
Duc Nguyen <ducntq@gmail.com>
2013-12-02 10:03:33 +0700

    Fix frontend after restructure

--- src/zo2_hallo/index.php
+++ src/zo2_hallo/index.php
@@ -53,10 +53,6 @@ $social = $zo2->zo2Social;
     <?php endif; ?>
     <?php echo $header?>
     <jdoc:include type="head" />
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body class="<?php echo $layout->getBodyClass()?>">
 <?php echo $social->renderFloatSocial() ?>
