2512cc5c11610ef9477ff16a5a5ee493e13a2881
Viet Vu <jooservices@gmail.com>
2013-12-06 23:39:39 +0700

    updated defines

--- src/zo2/includes/defines.php
+++ src/zo2/includes/defines.php
@@ -18,11 +18,14 @@ define('ZO2', 'zo2');
  * Paths
  */
 define('ZO2PATH_ROOT', realpath(dirname(__FILE__) . '/..'));
-define('ZO2PATH_ASSETS', ZO2PATH_ROOT .'/assets');
+define('ZO2PATH_ASSETS', ZO2PATH_ROOT . '/assets');
+define('ZO2PATH_ASSETS_ZO2', ZO2PATH_ASSETS . '/zo2');
+define('ZO2PATH_ASSETS_VENDOR', ZO2PATH_ASSETS . '/vendor');
+define('ZO2PATH_CACHE', JPATH_ROOT . '/cache/zo2');
 /**
  * Urls
  */
-define('ZO2URL_ROOT', rtrim(JUri::root(),'/') . '/plugins/system/' . ZO2);
+define('ZO2URL_ROOT', rtrim(JUri::root(), '/') . '/plugins/system/' . ZO2);
 define('ZO2URL_ASSETS', ZO2URL_ROOT . '/assets');
 define('ZO2URL_ASSETS_ZO2', ZO2URL_ASSETS . '/zo2');
 define('ZO2URL_ASSETS_VENDOR', ZO2URL_ASSETS . '/vendor');
