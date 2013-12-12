57a28abea66580a2f89f920c881e4700d92bbd1f
Duc Nguyen <ducntq@gmail.com>
2013-12-12 16:09:46 +0700

    Fix css path

--- src/zo2_hallo/layouts/cache/footer.php
+++ src/zo2_hallo/layouts/cache/footer.php
@@ -1,3 +1,11 @@
-<script type="text/javascript" src="templates/zo2_hallo/assets/js/ekko-lightbox.js"></script>
-<script type="text/javascript" src="templates/zo2_hallo/assets/js/megamenu.js"></script>
-<script type="text/javascript">jQuery(document).ready(function($){window.$ = jQuery});</script>
+<script type="text/javascript" src="http://zo2framework.local/templates/zo2_hallo/assets/js/ekko-lightbox.js"></script>
+<script type="text/javascript" src="/plugins/system/zo2/assets/vendor/bootstrap/core/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://zo2framework.local/templates/zo2_hallo/assets/js/megamenu.js"></script>
+<script type="text/javascript">jQuery("#gototop").click(function(){jQuery("body, html").animate({scrollTop: 0}); return false;});var scrollDiv = document.createElement("div");
+            jQuery(window).scroll(function () {
+                if (jQuery(this).scrollTop() != 0) {
+                    jQuery("#gototop").fadeIn();
+                } else {
+                    jQuery("#gototop").fadeOut();
+                }
+            });</script>
--- src/zo2_hallo/layouts/cache/header.php
+++ src/zo2_hallo/layouts/cache/header.php
@@ -1,6 +1,9 @@
-<link rel="stylesheet" href="templates/zo2_hallo/assets/css/normalize.css" type="text/css" />
-<link rel="stylesheet" href="templates/zo2_hallo/assets/css/template.css" type="text/css" />
-<link rel="stylesheet" href="templates/zo2_hallo/assets/css/presets/black.css" type="text/css" />
+<script type="text/javascript" src="http://zo2framework.local/templates/zo2_hallo/assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="http://zo2framework.local/templates/zo2_hallo/assets/js/jquery-noconflict.js"></script>
+<link rel="stylesheet" href="http://zo2framework.local/templates/zo2_hallo/assets/css/normalize.css" type="text/css" />
+<link rel="stylesheet" href="/plugins/system/zo2/assets/vendor/bootstrap/core/css/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="/plugins/system/zo2/assets/vendor/bootstrap/addons/font-awesome/css/font-awesome.min.css" type="text/css" />
+<link rel="stylesheet" href="http://zo2framework.local/templates/zo2_hallo/assets/css/template.css" type="text/css" />
 
 <style type="text/css">.logo_retina { display: none; }
                 .logo_normal {display:block}
@@ -11,5 +14,5 @@
                     .logo_normal { display: none; }
                     .logo_retina { display: block; }
                 }
-body{background-color:#dedede}#zo2-header{background-color:#cfcfcf}body{color:#ffffff}a{color:#212121}a:hover{color:#5eaf28}
+
 </style>
