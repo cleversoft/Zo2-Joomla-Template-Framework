afa59e0d70da293c954e98abc42e77aed970a5db
Viet Vu <jooservices@gmail.com>
2013-12-17 09:13:18 +0700

    + adding more shortcodes

---
+++ src/zo2/libraries/shortcodes/html/space.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Zo2 (http://www.zo2framework.org)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zo2framework.org
+ * @link        http://github.com/aploss/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+?>
+<div style="clear:both; height:<?php echo $params->height; ?>px;"></div>
