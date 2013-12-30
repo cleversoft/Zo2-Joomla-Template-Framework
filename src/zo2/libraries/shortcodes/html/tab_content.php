c360463595e1c842f691aae6bfa91c0888c21887
Viet Vu <jooservices@gmail.com>
2013-12-30 10:55:52 +0700

    + add tabs shortcodes

---
+++ src/zo2/libraries/shortcodes/html/tab_content.php
@@ -0,0 +1,3 @@
+<div class="tab-pane" id="<?php echo $this->get('name'); ?>">
+    <?php echo $this->get('content'); ?>
+</div>
---
+++ src/zo2/libraries/shortcodes/html/tab_title.php
@@ -0,0 +1,1 @@
+<li><a href="#<?php echo $this->get('name'); ?>" data-toggle="tab"><?php echo $this->get('content'); ?></a></li>
