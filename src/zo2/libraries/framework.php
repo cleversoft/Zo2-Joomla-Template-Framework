0b6e7440158dca3e82329c3c2fb6d5f6d9ccc8ac
Viet Vu <jooservices@gmail.com>
2014-01-03 09:14:58 +0700

    #144 RTL language

--- src/zo2/libraries/framework.php
+++ src/zo2/libraries/framework.php
@@ -142,7 +142,8 @@ if (!class_exists('Zo2Framework')) {
                     $assets->addScript('vendor/jquery/jquery-1.9.1.min.js');
                     $assets->addScript('vendor/jquery/jquery.noConflict.js');
                 }
-                if (self::get('enable_rtl') == 1)
+
+                if (self::get('enable_rtl') == 1 && JFactory::getDocument()->direction == 'rtl')
                     $assets->addStyleSheet('vendor/bootstrap/addons/bootstrap-rtl/css/bootstrap-rtl.css');
 
                 /* Load Boostrap */
@@ -160,13 +161,12 @@ if (!class_exists('Zo2Framework')) {
 
                 // template assets
                 self::prepareTemplateAssets();
-                
+
                 // presets
                 self::preparePresets();
 
                 // custom fonts
                 self::prepareCustomFonts();
-
             }
         }
 
