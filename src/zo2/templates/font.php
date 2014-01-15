fdbb13fd2aab9ce4a571e8aea0b1694fb6750014
Viet Vu <jooservices@gmail.com>
2014-01-15 14:02:50 +0700

    #167 Fonts

--- src/zo2/templates/font.php
+++ src/zo2/templates/font.php
@@ -9,10 +9,30 @@
  * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
  * @license     GPL v2
  */
-defined('_JEXEC') or die ('Restricted access');
+defined('_JEXEC') or die('Restricted access');
 /* @var $this JFormFieldFont */
 $data = null;
-if (!empty($this->value)) $data = json_decode($this->value, true);
+$enable = false;
+if (!empty($this->value)) {
+    $data = json_decode($this->value, true);
+    $enable = true;
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+}
+/**
+ * Init default data
+ */
+if (!isset($data['type']))
+    $data['type'] = 'standard';
+if (!isset($data['size']))
+    $data['size'] = null;
+if (!isset($data['color']))
+    $data['color'] = null;
+if (!isset($data['style']))
+    $data['style'] = null;
+if (!isset($data['family']))
+    $data['family'] = null;
 
 $standardFonts = array(
     '\'Helvetica Neue\', Helvetica',
@@ -30,84 +50,85 @@ $fontStyles = array(
 );
 ?>
 <div class="font-container">
-    <input type="hidden" value="<?php echo htmlspecialchars($this->value)?>" name="<?php echo $this->name?>" id="<?php echo $this->id?>" />
-    <h3><?php echo $this->getLabel()?></h3>
+    <input type="hidden" value="<?php echo htmlspecialchars($this->value) ?>" name="<?php echo $this->name ?>" id="<?php echo $this->id ?>" />
+    <h3><?php echo $this->getLabel() ?></h3>
     <div class="control-group">
         <div class="control-label">
             <div class="font-label">Enable</div>
         </div>
         <div class="controls btn-group btn-group-onoff cbEnableFont">
-            <button class="btn btn-on <?php echo $data ? 'active btn-success' : ''?>">On</button>
-            <button class="btn btn-off <?php echo !$data ? 'active btn-danger' : ''?>">Off</button>
+            <button class="btn btn-on <?php echo $enable ? 'active btn-success' : '' ?>">On</button>
+            <button class="btn btn-off <?php echo!$enable ? 'active btn-danger' : '' ?>">Off</button>
         </div>
     </div>
     <div class="font_options" <?php echo $data ? 'style="display:block"' : 'style="display:none"' ?>>
         <div class="control-group">
             <div class="control-label">
                 <div class="font-label">Font type</div>
-                <div class="font-desc">Choose the type of font you want to use for <?php echo strtolower($this->getLabel())?></div>
+                <div class="font-desc">Choose the type of font you want to use for <?php echo strtolower($this->getLabel()) ?></div>
             </div>
             <div class="controls">
                 <div class="btn-group font-types" data-toggle="buttons-radio">
-                    <button type="button" class="btn btnStandardFonts <?php echo $data['type'] == 'standard' ? 'active btn-success' : ''?>">Standard Fonts</button>
-                    <button type="button" class="btn btnGoogleFonts <?php echo $data['type'] == 'googlefonts' ? 'active btn-success' : ''?>">Google Fonts</button>
-                    <button type="button" class="btn btnFontDeck <?php echo $data['type'] == 'fontdeck' ? 'active btn-success' : ''?>">FontDeck</button>
+                    <button type="button" class="btn btnStandardFonts <?php echo $data['type'] == 'standard' ? 'active btn-success' : '' ?>">Standard Fonts</button>
+                    <button type="button" class="btn btnGoogleFonts <?php echo $data['type'] == 'googlefonts' ? 'active btn-success' : '' ?>">Google Fonts</button>
+                    <button type="button" class="btn btnFontDeck <?php echo $data['type'] == 'fontdeck' ? 'active btn-success' : '' ?>">FontDeck</button>
                 </div>
             </div>
         </div>
 
-        <div class="font-options-standard control-group" <?php echo $data['type'] == 'standard' ? 'style="display:block"' : ''?>>
+        <div class="font-options-standard control-group" <?php echo $data['type'] == 'standard' ? 'style="display:block"' : '' ?>>
             <div class="control-label">
                 <div class="font-label">Standard Font</div>
-                <div class="font-desc">Choose the font face that is used for <?php echo strtolower($this->getLabel())?></div>
+                <div class="font-desc">Choose the font face that is used for <?php echo strtolower($this->getLabel()) ?></div>
             </div>
             <div class="controls">
                 <select class="ddlStandardFont show">
-                    <?php foreach($standardFonts as $font) : ?>
-                    <option <?php echo $data['family'] == $font ? 'selected' : ''?> value="<?php echo htmlspecialchars($font)?>"><?php echo htmlspecialchars($font)?></option>
-                    <?php endforeach;?>
+                    <?php foreach ($standardFonts as $font) : ?>
+                        <option <?php echo $data['family'] == $font ? 'selected' : '' ?> value="<?php echo htmlspecialchars($font) ?>"><?php echo htmlspecialchars($font) ?></option>
+                    <?php endforeach; ?>
                 </select>
             </div>
         </div>
 
-        <div class="font-options-google hide control-group" <?php echo $data['type'] == 'googlefonts' ? 'style="display:block"' : ''?>>
+        <div class="font-options-google hide control-group" <?php echo $data['type'] == 'googlefonts' ? 'style="display:block"' : '' ?>>
             <div class="control-label">
                 <div class="font-label">Google Font</div>
-                <div class="font-desc">Choose the type of font you want to use for <?php echo strtolower($this->getLabel())?></div>
+                <div class="font-desc">Choose the type of font you want to use for <?php echo strtolower($this->getLabel()) ?></div>
             </div>
             <div class="controls">
-                <input type="text" class="txtGoogleFontSelect" value="<?php echo $data['type'] == 'googlefonts' ? $data['family'] : ''?>" />
+                <input type="text" class="txtGoogleFontSelect" value="<?php echo $data['type'] == 'googlefonts' ? $data['family'] : '' ?>" />
             </div>
         </div>
 
-        <div class="font-options-fontdeck hide control-group" <?php echo $data['type'] == 'fontdeck' ? 'style="display:block"' : ''?>>
+        <div class="font-options-fontdeck hide control-group" <?php echo $data['type'] == 'fontdeck' ? 'style="display:block"' : '' ?>>
             <div class="control-label">
                 <div class="font-label">FontDeck Name</div>
                 <div class="font-desc">Paste the font family attribute from CSS code in Step 2 of FontDeck website here</div>
             </div>
             <div class="controls">
-                <textarea class="txtFontDeckCss"><?php echo $data['type'] == 'fontdeck' ? $data['family'] : ''?></textarea>
+                <textarea class="txtFontDeckCss"><?php echo $data['type'] == 'fontdeck' ? $data['family'] : '' ?></textarea>
             </div>
         </div>
 
         <div class="control-group">
             <div class="control-label">
                 <div class="font-label">Font options</div>
-                <div class="font-desc">Specify the <?php echo strtolower($this->getLabel())?> font properties</div>
+                <div class="font-desc">Specify the <?php echo strtolower($this->getLabel()) ?> font properties</div>
             </div>
             <div class="controls floatdiv clearfix">
-                <div><input type="text" class="txtFontSize" value="<?php echo $data['size']?>" /> px</div>
+                <div><input type="text" class="txtFontSize" value="<?php echo $data['size'] ?>" /> px</div>
 
                 <div class="colorpicker-container">
-                    <input type="text" class="txtColorPicker" value="<?php echo $data['color']?>" />
-                    <span class="color-preview" style="background-color: <?php echo empty($data['color']) ? 'transparent' : $data['color']?>"></span>
+                    <input type="text" class="txtColorPicker" value="<?php echo $data['color'] ?>" />
+                    <span class="color-preview" style="background-color: <?php echo empty($data['color']) ? 'transparent' : $data['color'] ?>"></span>
                 </div>
 
                 <div>
                     <select class="ddlFontStyle">
-                        <?php foreach ($fontStyles as $style => $title) : ?>
-                        <option <?php $data['style'] == $style ? 'selected' : ''?> value="<?php echo $style?>"><?php echo $title?></option>
-                        <?php endforeach; ?>
+                        <?php foreach ($fontStyles as $style => $title) { ?>
+                            <?php $selected = trim($data['style']) == trim($style); ?>
+                            <option <?php echo ($selected) ? 'selected' : '' ?> value="<?php echo $style ?>"><?php echo $title ?></option>
+                        <?php } ?>
                     </select>
                 </div>
             </div>
