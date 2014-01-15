849962c6f7df1a497423b625c737bcf579535ef9
Viet Vu <jooservices@gmail.com>
2014-01-15 16:06:00 +0700

    #170 default active tab

--- src/zo2/assets/zo2/js/scripts.js
+++ src/zo2/assets/zo2/js/scripts.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
     /* Colorbox */
     jQuery('a.colorbox').colorbox();
+    /* Default active tab */
+    jQuery('ul.nav-tabs a:first').tab('show') // Select first tab
 })
