d97c5919b43ae42b0d8a5673d9d85e03a04bb652
Viet Vu <jooservices@gmail.com>
2014-01-18 09:01:54 +0700

    ^ update scripts.js not call colorbox init if not loaded

--- src/zo2/assets/zo2/js/scripts.js
+++ src/zo2/assets/zo2/js/scripts.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function() {
-    /* Colorbox */
-    jQuery('a.colorbox').colorbox();
+    if (jQuery().colorbox) {
+        /* Colorbox */
+        jQuery('a.colorbox').colorbox();
+    }
     /* Default active tab for shortcode tabs*/
     jQuery('ul.nav-tabs.shortcode a:first').tab('show') // Select first tab
 })
