327cf9c61f96e7d7ba437ff8df94571a8c82d7da
khangvu <khangvm530@gmail.com>
2014-02-20 16:12:17 +0700

    change logo path to url

--- src/zo2_hallo/components/header_logo.php
+++ src/zo2_hallo/components/header_logo.php
@@ -53,10 +53,10 @@ class Zo2Component_header_logo extends Zo2Component {
                 $html .= htmlspecialchars($logo['text']);
             } else {
                 if ($logo['type'] == 'image' && !empty($logo['path'])) {
-                    $html .= '<a style="width: ' . $logo['width'] . 'px; height: ' . $logo['height'] . 'px;background-image: url(' . JUri::root(true) . '/' . $logo['path'] . ')" class="logo_normal" href="' . JUri::root(true) . '" title="' . (!empty($sitename) ? $sitename : '') . '"></a>';
+                    $html .= '<a style="width: ' . $logo['width'] . 'px; height: ' . $logo['height'] . 'px;background-image: url(' . JUri::root() . '/' . $logo['path'] . ')" class="logo_normal" href="' . JUri::root() . '" title="' . (!empty($sitename) ? $sitename : '') . '"></a>';
                 }
                 if ($logoRetina['type'] == 'image' && !empty($logoRetina['path'])) {
-                    $html .= '<a style="width: ' . $logoRetina['width'] . 'px; height: ' . $logoRetina['height'] . 'px;background-image: url(' . JUri::root(true) . '/' . $logoRetina['path'] . ')" class="logo_retina" href="' . JUri::root(true) . '/' . '" title="' . (!empty($sitename) ? $sitename : '') . '"></a>';
+                    $html .= '<a style="width: ' . $logoRetina['width'] . 'px; height: ' . $logoRetina['height'] . 'px;background-image: url(' . JUri::root() . '/' . $logoRetina['path'] . ')" class="logo_retina" href="' . JUri::root() . '/' . '" title="' . (!empty($sitename) ? $sitename : '') . '"></a>';
                 }
             }
             $html .=!empty($slogan) ? '<h2 class="header_slogan">' . $slogan . '</h2>' : '';
