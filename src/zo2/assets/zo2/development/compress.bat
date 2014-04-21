58067dc7bc8d49778c74a59c8de0caa0b83f934d
Viet Vu <jooservices@gmail.com>
2014-04-21 09:05:23 +0700

    - remove compressor by java

--- src/zo2/assets/zo2/development/compress.bat
+++
@@ -1,5 +0,0 @@
-java -jar yuicompressor-2.4.8.jar -o ..\js\admin.megamenu.js js\admin.megamenu.js
-java -jar yuicompressor-2.4.8.jar -o ..\js\admin.social.js js\admin.social.js
-java -jar yuicompressor-2.4.8.jar -o ..\js\admin.scripts.js js\admin.scripts.js
-java -jar yuicompressor-2.4.8.jar -o ..\js\site.scripts.js js\site.scripts.js
-java -jar yuicompressor-2.4.8.jar -o ..\js\site.megamenu.js js\site.megamenu.js
