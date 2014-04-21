dd12500d05d9f282dd6efae7f6388e1b041f6f09
Viet Vu <jooservices@gmail.com>
2014-04-21 09:04:22 +0700

    - remove old socialshares

--- src/zo2/libraries/socialshares/reddit.php
+++
@@ -1,7 +0,0 @@
-<div class="zo2-button-reddit share">
-    <a href="http://www.reddit.com/submit" 
-       onclick="window.location = 'http://www.reddit.com/submit?url=' + encodeURIComponent(<?php echo $this->get('url'); ?>);
-               return false">
-        <img src="http://www.reddit.com/static/<?php echo $this->get('icon'); ?>" alt="submit to reddit" border="0" />
-    </a>
-</div>
