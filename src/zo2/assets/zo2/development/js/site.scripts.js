664c2682a417d3cb5ce0ebf7857890d17a962f94
khangvu <khangvm530@gmail.com>
2014-04-26 21:06:56 +0700

    fix select element from url

--- src/zo2/assets/zo2/development/js/site.scripts.js
+++ src/zo2/assets/zo2/development/js/site.scripts.js
@@ -9,11 +9,11 @@ jQuery(document).ready(function() {
     jQuery("#zo2-header").sticky({topSpacing:0});
 
     jQuery('.zo2-megamenu .navbar-nav a').click(function(){
-        var href = jQuery(this).attr('href');
-        var elements = href.split('#');
-        alert('#'+elements[elements.length -1 ]);
+        var element = jQuery(this).attr('href');
+        //var elements = href.split('#');
+        //alert('#'+elements[elements.length -1 ]);
         jQuery('html, body').animate({
-            scrollTop: jQuery('#'+elements[elements.length -1 ]).offset().top
+            scrollTop: jQuery(element).offset().top
         }, 800);
     });
 
