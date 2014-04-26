f0f84c40206d39e8ba0ff6903162aff46b42abcd
khangvu <khangvm530@gmail.com>
2014-04-26 21:05:53 +0700

    add one page animate

--- src/zo2/assets/zo2/development/js/site.scripts.js
+++ src/zo2/assets/zo2/development/js/site.scripts.js
@@ -8,4 +8,13 @@ jQuery(document).ready(function() {
 
     jQuery("#zo2-header").sticky({topSpacing:0});
 
+    jQuery('.zo2-megamenu .navbar-nav a').click(function(){
+        var href = jQuery(this).attr('href');
+        var elements = href.split('#');
+        alert('#'+elements[elements.length -1 ]);
+        jQuery('html, body').animate({
+            scrollTop: jQuery('#'+elements[elements.length -1 ]).offset().top
+        }, 800);
+    });
+
 });
