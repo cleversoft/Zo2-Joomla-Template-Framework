8434fdaf1dd491c5b4ef81475743f5db1ec4f87d
datlt89 <ledat.apt@gmail.com>
2014-05-26 16:36:53 +0700

    - Developement/less: edit css - images\background-patterns: change background pattern - component.php : fix joomla editor warning - error.php: fix 404 page - templateDetails.xml: fix link logo, favicon

--- src/zo2_hallo/component.php
+++ src/zo2_hallo/component.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-    <?php echo $header?>
+    <?php echo isset($header) ? $header : "" ?>
     <jdoc:include type="head" />
     <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/zo2_hallo/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/zo2_hallo/assets/css/component.css" type="text/css" />
