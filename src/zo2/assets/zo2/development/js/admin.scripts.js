2eea59c74413bd84337fac169d1bc77afdb4fbed
khangvu <khangvm530@gmail.com>
2014-05-29 18:10:57 +0700

    change style of radio button in joomla 2.5

--- src/zo2/assets/zo2/development/js/admin.scripts.js
+++ src/zo2/assets/zo2/development/js/admin.scripts.js
@@ -32,6 +32,41 @@ $(window).bind('load', function() {
 
 jQuery(document).ready(function($) {
 
+    /*============For joomla 2.5==============*/
+    // Turn radios into btn-group
+    $('.radio.btn-group input[type="radio"]').hide();
+
+    $('.radio.btn-group label').addClass('btn');
+
+    $('.btn-group label:not(.active)').click(function()
+    {
+        var label = $(this);
+        var input = $('#' + label.attr('for'));
+
+        if (!input.prop('checked')) {
+            label.closest('.btn-group').find('label').removeClass('active btn-success btn-danger btn-primary');
+            if (input.val() == '') {
+                label.addClass('active btn-primary');
+            } else if (input.val() == 0) {
+                label.addClass('active btn-danger');
+            } else {
+                label.addClass('active btn-success');
+            }
+            input.prop('checked', true);
+        }
+    });
+    $('.btn-group input[checked=checked]').each(function()
+    {
+        if ($(this).val() == '') {
+            $('label[for=' + $(this).attr('id') + ']').addClass('active btn-primary');
+        } else if ($(this).val() == 0) {
+            $('label[for=' + $(this).attr('id') + ']').addClass('active btn-danger');
+        } else {
+            $('label[for=' + $(this).attr('id') + ']').addClass('active btn-success');
+        }
+    });
+
+    /*============For all==============*/
     injectFormSubmit();
     //wrapForm();
 
@@ -630,7 +665,7 @@ var generatePresetData = function() {
         footer: $('#color_footer').val(),
         extra: JSON.stringify(extra),
         bg_image: $('#zo2_background_image').val(),
-        bg_pattern: $('.background-select li.selected img').attr('rel')
+        bg_pattern: $('.background-select li.selected img').attr('src')
     };
 
     $('#zo2_themes_container').find('input:first').val(JSON.stringify(data));
