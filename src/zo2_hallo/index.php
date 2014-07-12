c4e1d4f6e1b7d632bccb906a595e4f7e0abad724
Viet Vu <jooservices@gmail.com>
2014-07-12 11:51:32 +0700

    - remove hard code for canvas menu

--- src/zo2_hallo/index.php
+++ src/zo2_hallo/index.php
@@ -18,33 +18,31 @@ require_once __DIR__ . '/includes/bootstrap.php';
  * @todo Opengraph support
  * @todo Facebook & Twitter ... data attributes support
  */
-
 /* Get Zo2Framework */
 $framework = Zo2Factory::getFramework();
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->zo2->template->getLanguage(); ?>" dir="<?php echo $this->zo2->template->getDirection(); ?>">
-<head>
-    <?php unset($this->_scripts[JURI::root(true).'/media/jui/js/bootstrap.min.js']); ?>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <?php echo $this->zo2->template->fetch('html://layouts/head.response.php'); ?>
-    <?php echo $this->zo2->template->fetch('html://layouts/head.favicon.php'); ?>
-    <!--[if !IE 8]> -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-    <!-- <![endif]-->
+    <head>
+        <?php unset($this->_scripts[JURI::root(true) . '/media/jui/js/bootstrap.min.js']); ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <?php echo $this->zo2->template->fetch('html://layouts/head.response.php'); ?>
+        <?php echo $this->zo2->template->fetch('html://layouts/head.favicon.php'); ?>
+        <!--[if !IE 8]> -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+        <!-- <![endif]-->
     <jdoc:include type="head" />
 </head>
 <body class="<?php echo $this->zo2->layout->getBodyClass(); ?> <?php echo $this->zo2->template->getDirection(); ?> <?php echo $this->zo2->framework->isBoxed() ? 'boxed' : ''; ?>">
-<?php echo $this->zo2->template->fetch('html://layouts/css.condition.php'); ?>
-<?php echo $framework->displayOffCanvasMenu() ?>
-<section class="wrapper <?php echo $this->zo2->framework->isBoxed() ? 'boxed container' : ''; ?>">
-    <?php //echo $this->zo2->utilities->socialshares->render('floatbar'); ?>
-    <?php echo $this->zo2->utilities->styleswitcher->render(); ?>
-    <?php echo $this->zo2->layout->render(); ?>
-</section>
-<?php echo $this->zo2->template->fetch('html://layouts/joomla.debug.php'); ?>
-<script>
-    <?php echo $this->zo2->utilities->bottomscript->render(); ?>
-</script>
+    <?php echo $this->zo2->template->fetch('html://layouts/css.condition.php'); ?>
+    <section class="wrapper <?php echo $this->zo2->framework->isBoxed() ? 'boxed container' : ''; ?>">
+        <?php //echo $this->zo2->utilities->socialshares->render('floatbar');  ?>
+        <?php echo $this->zo2->utilities->styleswitcher->render(); ?>
+        <?php echo $this->zo2->layout->render(); ?>
+    </section>
+    <?php echo $this->zo2->template->fetch('html://layouts/joomla.debug.php'); ?>
+    <script>
+<?php echo $this->zo2->utilities->bottomscript->render(); ?>
+    </script>
 </body>
 </html>
