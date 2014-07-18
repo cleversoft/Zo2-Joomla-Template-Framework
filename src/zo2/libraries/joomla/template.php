9d516ee0b6c6582006df906830afe9708e7b5833
Viet Vu <jooservices@gmail.com>
2014-07-18 09:16:42 +0700

    ^ fix hook template saving process

--- src/zo2/libraries/joomla/template.php
+++ src/zo2/libraries/joomla/template.php
@@ -16,8 +16,26 @@ if (!class_exists('Zo2JTemplate')) {
 
     class Zo2JTemplate {
 
+        protected $_jinput;
+
+        public function __construct() {
+            $this->_jinput = JFactory::getApplication()->input;
+        }
+
+        /**
+         * 
+         */
         public function process() {
-            $this->save();
+            if ($this->_jinput->get('option') == 'com_templates') {
+                switch ($this->_jinput->get('task')) {
+                    case 'style.save':
+                        $this->save();
+                        break;
+                    case 'style.apply':
+                        $this->save();
+                        break;
+                }
+            }
         }
 
         /**
@@ -26,69 +44,64 @@ if (!class_exists('Zo2JTemplate')) {
         public function save() {
             /* Hook and replace Joomla! style save */
             $jinput = JFactory::getApplication()->input;
-            if ($jinput->get('option') == 'com_templates') {
-                /**
-                 * @todo Replace by JInput
-                 */
-                if (isset($_REQUEST['jform'])) {
-                    JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_templates/models');
-                    JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_templates/tables');
 
-                    $lang = JFactory::getLanguage();
-                    $extension = 'com_templates';
-                    $base_dir = JPATH_ADMINISTRATOR;
-                    $language_tag = 'en-GB';
-                    $reload = true;
-                    $lang->load($extension, $base_dir, $language_tag, $reload);
+            /**
+             * @todo Replace by JInput
+             */
+            if (isset($_REQUEST['jform'])) {
+                JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_templates/models');
+                JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_templates/tables');
 
-                    /* Get table */
-                    $table = JTable::getInstance('Style', 'TemplatesTable');
-                    /* Do never use $_REQUEST */
-                    $data = $jinput->post->get('jform', array(), 'array');
+                $lang = JFactory::getLanguage();
+                $extension = 'com_templates';
+                $base_dir = JPATH_ADMINISTRATOR;
+                $language_tag = 'en-GB';
+                $reload = true;
+                $lang->load($extension, $base_dir, $language_tag, $reload);
 
-                    /* Save template with data */
-                    $model = JModelLegacy::getInstance('Style', 'TemplatesModel');
-                    $model->save($data);
-                    
-                    if ($table->load(array(
-                                'template' => $data['template'],
-                                'client_id' => $data['client_id'],
-                                'home' => $data['home'],
-                                'title' => $data['title']
-                            ))) {
-                        $params = new JRegistry($data['params']);
-                        $table->params = $params->toString();
-                        if ($table->check()) {
-                            if ($table->store()) {
+                /* Get table */
+                $table = JTable::getInstance('Style', 'TemplatesTable');
+                /* Do never use $_REQUEST */
+                $data = $jinput->post->get('jform', array(), 'array');
 
-                                /**
-                                 * Save profile
-                                 */
-                                $profile = new Zo2Profile();
-                                $profile->template = $data['template'];
-                                $profile->name = $params->get('profile', 'default');
-                                $profile->layout = json_decode($params->get('layout'));
-                                $profile->save();
-                               
-                                $application = JFactory::getApplication();
-                                $application->enqueueMessage('Style successfully saved');
+                /* Save template with data */
+                $model = JModelLegacy::getInstance('Style', 'TemplatesModel');
+                $model->save($data);
 
-                                if ($jinput->get('task') == 'style.apply') {
-                                    $application->redirect(JRoute::_('index.php?option=com_templates&view=style&layout=edit&id=' . $table->id, false));
-                                } else {
-                                    $application->redirect(JRoute::_('index.php?option=com_templates&view=styles', false));
-                                }
+                if ($table->load(array(
+                            'template' => $data['template'],
+                            'client_id' => $data['client_id'],
+                            'home' => $data['home'],
+                            'title' => $data['title']
+                        ))) {
+                    $params = new JRegistry($data['params']);
+                    $table->params = $params->toString();
+                    if ($table->check()) {
+                        if ($table->store()) {
+
+                            /**
+                             * Save profile
+                             */
+                            $profile = new Zo2Profile();
+                            $profile->template = $data['template'];
+                            $profile->name = $params->get('profile', 'default');
+                            $profile->layout = json_decode($params->get('layout'));
+                            $profile->save();
+
+                            $application = JFactory::getApplication();
+                            $application->enqueueMessage('Style successfully saved');
+
+                            if ($jinput->get('task') == 'style.apply') {
+                                $application->redirect(JRoute::_('index.php?option=com_templates&view=style&layout=edit&id=' . $table->id, false));
+                            } else {
+                                $application->redirect(JRoute::_('index.php?option=com_templates&view=styles', false));
                             }
                         }
-                    } else {
-                        JFactory::getApplication()->enqueueMessage('Style save error');
                     }
+                } else {
+                    JFactory::getApplication()->enqueueMessage('Style save error');
                 }
             }
-        }
-
-        public function apply() {
-            
         }
 
     }
