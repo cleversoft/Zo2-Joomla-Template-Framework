df2004b026795a703c495740bfda3b4ae48d95a1
khangvm530@gmail.com <mua2tang1>
2014-07-28 18:04:05 +0700

    fixed image footer and gototop icon

--- plugins/system/zo2/html/layouts/copyright.php
+++ plugins/system/zo2/html/layouts/copyright.php
@@ -9,11 +9,11 @@ $html = '<footer>';
 $html .= '<section class="copyright" style="text-align:center">' . $copyright . '</section>';
 if ($logo == 1) {
     $html .= '<a title="Powered by Zo2Framework" class="footer_zo2_logo" href="http://zo2framework.org" style="display:block;">';
-    $html .= '<img src="' . JUri::root(true) . '/plugins/system/zo2/assets/images/zo2logo.png" />';
+    $html .= '<img src="' . JUri::root(true) . '/plugins/system/zo2/assets/zo2/images/zo2logo.png" />';
     $html .= '</a>';
 }
 if ($gototop) {
-    $html .= '<a href="#" id="gototop" title="Go to top"><i class="icon-chevron-up"></i></a>';
+    $html .= '<a href="#" id="gototop" title="Go to top"><i class="fa fa-chevron-up"></i></a>';
 
     $script = 'jQuery("#gototop").click(function(){jQuery("body, html").animate({scrollTop: 0}); return false;});';
 
