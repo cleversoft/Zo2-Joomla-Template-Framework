4bdecbdd8ea25e347abc2d915b728c1bc3c05795
Viet Vu <jooservices@gmail.com>
2014-10-18 09:48:06 +0700

    ^ update main scripts

--- plugins/system/zo2/framework/assets/zo2/development/js/scripts.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/scripts.js
@@ -1,4 +1,31 @@
 /**
+ * Zo2 JS framework define
+ * @version <?php //echo CREX_VERSION; ?>
+ * @param {object} w Window pointer
+ * @param {object} $ jQuery pointer
+ * @returns {undefined}
+ */
+(function (w, $) {
+    if (typeof w.zo2 === 'undefined') {
+        /* Local zo2 definition */
+        var _zo2 = {
+            /* Common settings */
+            settings: {
+            },
+            /* Internal jQuery */
+            jQuery: $
+
+        };
+
+        /* Provide global zo2 object */
+        w.zo2 = _zo2;
+
+    }
+
+})(window, jQuery);
+
+
+/**
  * Main script file
  * @param {object} window
  * @param {object} zo2
--- plugins/system/zo2/framework/assets/zo2/js/scripts.js
+++ plugins/system/zo2/framework/assets/zo2/js/scripts.js
@@ -1,7 +1,51 @@
+/**
+ * Zo2 JS framework define
+ * @version <?php //echo CREX_VERSION; ?>
+ * @param {object} w Window pointer
+ * @param {object} $ jQuery pointer
+ * @returns {undefined}
+ */
+(function (w, $) {
+    if (typeof w.zo2 === 'undefined') {
+        /* Local zo2 definition */
+        var _zo2 = {
+            /* Common settings */
+            settings: {
+            },
+            /* Internal jQuery */
+            jQuery: $
+
+        };
+
+        /* Provide global zo2 object */
+        w.zo2 = _zo2;
+
+    }
+
+})(window, jQuery);
+
+
+/**
+ * Main script file
+ * @param {object} window
+ * @param {object} zo2
+ * @param {object} $
+ * @returns {undefined}
+ */
 (function (window, zo2, $) {
+
+    /**
+     * Ajax class
+     */
     zo2.ajax = {
+        /* Default settings */
+        _settings: {
+        }
     };
 
+    /**
+     * Animation class
+     */
     zo2.animate = {
         /**
          * Scroll from el to toEl
@@ -12,7 +56,7 @@
         scrollTop: function (el, toEl) {
             $(el).animate({
                 scrollTop: jQuery(toEl).offset().top
-            }, 1500)
+            }, 1500);
         }
-    }
+    };
 })(window, zo2, zo2.jQuery);
