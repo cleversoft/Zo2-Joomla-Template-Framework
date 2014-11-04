ac23b03315f2c9f179764e9e1cfe22d7c15f8095
Viet Vu <jooservices@gmail.com>
2014-11-04 08:39:10 +0700

    - remove old files

--- plugins/system/zo2/framework/html/admin/body_profileassignment.php
+++
@@ -1,7 +0,0 @@
-<!-- Profile Assignment -->
-<div class="profiles-pane">
-    <h3 class="title-profile">Profile Assignment: Assign this layout profile to menu items.</h3>
-    <div class="profiles-pane-inner">
-        <?php echo $this->loadTemplate('assignment_profiles'); ?>
-    </div>
-</div>
