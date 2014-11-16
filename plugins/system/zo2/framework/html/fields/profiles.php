422a248e8a7582a06a64e8148bc8ccf3ae99929e
Viet Vu <jooservices@gmail.com>
2014-11-16 20:36:45 +0700

    ^ add function backup profiles

--- plugins/system/zo2/framework/html/fields/profiles.php
+++ plugins/system/zo2/framework/html/fields/profiles.php
@@ -27,60 +27,66 @@ defined('_JEXEC') or die('Restricted access');
             <?php endforeach; ?>
         </select>
 
-        <!-- Add new profile -->
-        <?php if ($this->data['profile']->authorise('add')) : ?>            
-            <div class="zo2-addProfile">            
-                <span class="input-group-btn">            
-                    <button class="btn btn-default" id="zo2-addProfile" onClick="jQuery('zo2-form-addProfile').toggle();
-                            return false;">Add</button> 
-                </span>            
-                <div class="zo2-form-addProfile" id="zo2-form-addProfile">
-                    <p>Please enter new profile name</p>
-                    <input type="text" id="zo2-profile-name" name="profile-name">
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" id="zo2-save-profile" onclick="zo2.admin.profile.add();
-                                return false;">Save</button>
-                        <button class="btn btn-default" id="zo2-cancel-profile" onclick="jQuery('zo2-form-addProfile').toggle();
-                                return false;">Cancel</button>
-                    </span>
-                </div>    
-            </div>
-        <?php endif; ?>
+        <div class="btn-toolbar" style="margin: 0;">
+            <div class="btn-group">
+                <!-- Add new profile -->
+                <?php if ($this->data['profile']->authorise('add')) : ?>            
+                    <div class="zo2-addProfile">            
+                        <span class="input-group-btn">            
+                            <button class="btn btn-default" id="zo2-addProfile" onClick="jQuery('zo2-form-addProfile').toggle();
+                                        return false;">Create new profile</button> 
+                        </span>            
+                        <div class="zo2-form-addProfile" id="zo2-form-addProfile">
+                            <p>Please enter new profile name</p>
+                            <input type="text" id="zo2-profile-name" name="profile-name">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" id="zo2-save-profile" onclick="zo2.admin.profile.add();
+                                            return false;">Save</button>
+                                <button class="btn btn-default" id="zo2-cancel-profile" onclick="jQuery('zo2-form-addProfile').toggle();
+                                            return false;">Cancel</button>
+                            </span>
+                        </div>    
+                    </div>
+                <?php endif; ?>
 
-        <!-- Rename profile -->
-        <?php if ($this->data['profile']->authorise('rename')) : ?>    
-            <div class="zo2-renameProfile">
-                <span class="input-group-btn">            
-                    <button class="btn btn-default" id="zo2-renameProfile" onClick="jQuery('zo2-form-renameProfile').toggle();
-                            return false;">Add</button> 
-                </span>  
-                <div class="zo2-form-newProfile" id="zo2-form-renameProfile" style="">
-                    <p>Please enter layout profile name</p>
-                    <input type="text" id="zo2-new-profile-name" name="newProfileName">
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" id="zo2-rename-profile" onclick="zo2.admin.profile.rename('<?php echo $this->data['profile']->name; ?>');
-                                return false;">Save</button>
-                        <button class="btn btn-default" id="zo2-cancel-rename-profile">Cancel</button>
-                    </span>
-                </div>
-            </div>
-        <?php endif; ?>
+                <!-- Rename profile -->
+                <?php if ($this->data['profile']->authorise('rename')) : ?>    
+                    <div class="zo2-renameProfile">
+                        <span class="input-group-btn">            
+                            <button class="btn btn-default" id="zo2-renameProfile" onClick="jQuery('zo2-form-renameProfile').toggle();
+                                        return false;">Rename</button> 
+                        </span>  
+                        <div class="zo2-form-newProfile" id="zo2-form-renameProfile" style="">
+                            <p>Please enter layout profile name</p>
+                            <input type="text" id="zo2-new-profile-name" name="newProfileName">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" id="zo2-rename-profile" onclick="zo2.admin.profile.rename('<?php echo $this->data['profile']->name; ?>');
+                                            return false;">Save</button>
+                                <button class="btn btn-default" id="zo2-cancel-rename-profile">Cancel</button>
+                            </span>
+                        </div>
+                    </div>
+                <?php endif; ?>
 
-        <!-- Remove profile -->
-        <?php if ($this->data['profile']->authorise('remove')) : ?>    
-            <div class="zo2-renameProfile">
-                <span class="input-group-btn">            
-                    <button class="btn btn-danger" id="zo2-renameProfile" onClick="zo2.admin.ajax.removeProfile();
-                            return false;">Remove</button> 
-                </span>                  
+                <!-- Remove profile -->
+                <?php if ($this->data['profile']->authorise('remove')) : ?>    
+                    <div class="zo2-renameProfile">
+                        <span class="input-group-btn">            
+                            <button class="btn btn-danger" id="zo2-renameProfile" onClick="zo2.admin.ajax.removeProfile();
+                                        return false;">Delete</button> 
+                        </span>                  
+                    </div>
+                <?php endif; ?>
             </div>
-        <?php endif; ?>
+
+        </div>
+
 
         <input type="hidden" name="profile-id" value="<?php echo Zo2Factory::getFramework()->get('id'); ?>">
     </div>
     <div class="row-fluid">
         <div class="span12">
-            <button class="btn btn-primary" id="btnBackupProfile" onClick="" data-toggle="button" data-loading-text="Processing..." >Backup</button>
+            <button class="btn btn-primary" id="btnBackupProfile" onClick="window.open('<?php echo JUri::getInstance()->toString() . '&zo2_task=downloadBackup&tmpl=raw'; ?>');return false;" data-toggle="button" data-loading-text="Processing..." >Backup</button>
             <button class="btn btn-danger" id="btnRestoreProfile" onClick="" data-toggle="button" data-loading-text="Processing..." >Restore</button>
         </div>
     </div>
