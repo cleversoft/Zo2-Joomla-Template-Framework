e6b89b93395dfed826da038eb51864428fb06d66
Dung Tran <tad88.dev@gmail.com>
2014-12-04 17:01:33 +0700

    ^clean & optimize admin.scripts.js

--- plugins/system/zo2/framework/assets/zo2/development/js/admin.scripts.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/admin.scripts.js
@@ -10,23 +10,24 @@
  */
 
 /**
- *
- * @param {type} window
- * @param {type} zo2
- * @param {type} $
+ * Zo2 Admin core
+ * @param {type} w Window pointer
+ * @param {type} z Zo2 pointer
+ * @param {type} $ pointer
  * @returns {undefined}
  */
-(function (window, zo2, $) {
+(function(w, z, $) {
+
     /**
      * Zo2 administrator object
      * Only use for backend
      */
-    zo2.admin = {
+    var _admin = {
         /**
          * Init function
          * @returns {undefined}
          */
-        _init: function () {
+        _init: function() {
 
         },
         /**
@@ -34,45 +35,47 @@
          * @returns {undefined}
          * @todo Need display notice message when success or fail
          */
-        clearCache: function () {
-            settings = {
+        clearCache: function() {
+            z.ajax.request({
                 data: {
                     zo2_task: 'admin.clearCache'
                 }
-            };
-            zo2.ajax.request(settings);
+            });
         },
         /**
-         *
+         * Assets builder
          * @returns {undefined}
          */
-        buildAssets: function () {           
-            settings = {
+        buildAssets: function() {
+            z.ajax.request({
                 data: {
                     zo2_task: 'admin.buildAssets'
                 }
-            };
-            zo2.ajax.request(settings);
+            });
         },
-        
-        generateZo2SettingJson: function () {
+        /**
+         * Generate JSON settings 
+         * @returns {unresolved}
+         */
+        generateZo2SettingJson: function() {
             var $rootParent = jQuery('#droppable-container .zo2-container');
             var json = [];
             /* Loop all rows */
-            $rootParent.find('>[data-zo2-type="row"]').each(function () {
-                var itemJson = zo2.admin.generateLayoutJson(jQuery(this));
+            $rootParent.find('>[data-zo2-type="row"]').each(function() {
+                var itemJson = z.admin.generateLayoutJson(jQuery(this));
                 if (itemJson != null)
                     json.push(itemJson);
             });
 
-            return JSON.stringify(json);
+            return w.JSON.stringify(json);
         },
         /**
-         *
+         * Generation default JSON settings
+         * @todo Use HTML5 data(), do not use attr()
          * @param {type} $item
          * @returns {generateLayoutJson.result}
          */
-        generateLayoutJson: function ($item) {
+        generateLayoutJson: function($item) {
             var result = null;
             var $childrenContainer = null;
             /* Row */
@@ -94,8 +97,8 @@
 
                 $childrenContainer = $item.find('> .row-control > .col-container');
 
-                $childrenContainer.find('> [data-zo2-type]').each(function () {
-                    var childItem = zo2.admin.generateLayoutJson(jQuery(this));
+                $childrenContainer.find('> [data-zo2-type]').each(function() {
+                    var childItem = z.admin.generateLayoutJson(jQuery(this));
                     result.children.push(childItem);
                 });
             }
@@ -122,21 +125,23 @@
 
                 $childrenContainer = $item.find('> .col-wrap > .row-container');
 
-                $childrenContainer.find('> [data-zo2-type]').each(function () {
-                    var childItem = zo2.admin.generateLayoutJson(jQuery(this));
+                $childrenContainer.find('> [data-zo2-type]').each(function() {
+                    var childItem = z.admin.generateLayoutJson(jQuery(this));
                     result.children.push(childItem);
                 });
             }
 
             return result;
         },
-        generateLogoJson: function ($container) {
-            var $buttons = $container.find('.logo-type-switcher').find('button');
+        /**
+         * Generate logo JSON
+         * @param {type} $container
+         * @returns {undefined}
+         */
+        generateLogoJson: function($container) {
             var $input = $container.find('.logoInput');
             var $activeButton = $container.find('.logo-type-switcher').find('button.active');
-
-            var data = {type: "none"};
-
+            var data = {};
             if ($activeButton.hasClass('logo-type-none')) {
                 data = {type: "none"};
             }
@@ -162,59 +167,65 @@
                     text: $container.find('.logo-text-input').val()
                 };
             }
-            //console.log(data);
-            $input.val(JSON.stringify(data));
+            $input.val(w.JSON.stringify(data));
 
         }
     };
+
+    /**
+     * Append admin to zo2
+     */
+    z.admin = _admin;
+
     /* Init Zo2.admin */
-    $(document).ready(function () {
-        zo2.admin._init();
+    $(w.document).ready(function() {
+        z.admin._init();
+    });
+
+
+    $(w.document).ready(function(e) {
+        /* Override default submit function */
+        w.Joomla.submitform = function(task, form) {
+            if (typeof (form) === 'undefined' || form === null) {
+                form = w.document.adminForm;
+            }
+            jQuery('.toolbox-saveConfig').trigger('click'); // dirty hack for megamenu save
+
+            if (typeof (task) !== 'undefined') {
+                form.task.value = task;
+            }
+
+            // Submit the form.
+            if (typeof form.onsubmit == 'function') {
+                form.onsubmit();
+            }
+            if (typeof form.fireEvent == "function") {
+                form.fireEvent('submit');
+            }
+
+            var $input = $('.hfLayoutHtml');
+            $('.field-logo-container').each(function() {
+                z.admin.generateLogoJson($(this));
+            });
+            $input.val(z.admin.generateZo2SettingJson());
+
+            form.submit();
+        };
+
+        /**
+         * @todo remove this code
+         */
+        $('#updater-desc a.btn-success').click(function(e) {
+            e.preventDefault();
+            jPrompt(jQuery(this).next().find('span').html() + '. Type "OK" to Continuous.', '', 'ZO2 Framework update confirmation box', function(confirm) {
+                if (confirm.toString().toLowerCase() === "ok") {
+                    w.location.href = jQuery('#updater-desc a.btn-success').attr('href');
+                } else {
+                    return false;
+                }
+            });
+        });
     });
 
 })(window, zo2, zo2.jQuery);
 
-zo2.jQuery(document).ready(function ($) {
-
-    /* Override default submit function */
-    Joomla.submitform = function (task, form) {
-        if (typeof (form) === 'undefined' || form === null) {
-            form = document.adminForm;
-        }
-        jQuery('.toolbox-saveConfig').trigger('click'); // dirty hack for megamenu save
-
-        if (typeof (task) !== 'undefined') {
-            form.task.value = task;
-        }
-
-        // Submit the form.
-        if (typeof form.onsubmit == 'function') {
-            form.onsubmit();
-        }
-        if (typeof form.fireEvent == "function") {
-            form.fireEvent('submit');
-        }
-
-        var $ = jQuery;
-        var $input = $('.hfLayoutHtml');
-        $('.field-logo-container').each(function () {
-            zo2.admin.generateLogoJson($(this));
-        });
-        $input.val(zo2.admin.generateZo2SettingJson());
-
-        form.submit();
-    };
-
-
-
-    jQuery('#updater-desc a.btn-success').click(function (e) {
-        e.preventDefault();
-        jPrompt(jQuery(this).next().find('span').html() + '. Type "OK" to Continuous.', '', 'ZO2 Framework update confirmation box', function (confirm) {
-            if (confirm == "OK" || confirm == "ok" || confirm == "oK" || confirm == "Ok") {
-                location.href = jQuery('#updater-desc a.btn-success').attr('href');
-            } else {
-                return false;
-            }
-        });
-    });
-});
--- plugins/system/zo2/framework/assets/zo2/development/js/zo2.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/zo2.js
@@ -1,4 +1,15 @@
 /**
+ * Zo2 (http://www.zootemplate.com/zo2)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zootemplate.com/zo2
+ * @link        https://github.com/cleversoft/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2014 CleverSoft (http://cleversoft.co/)
+ * @license     GPL v2
+ */
+
+/**
  * Zo2 JS framework define
  * @param {object} w Window pointer
  * @param {object} $ jQuery pointer
