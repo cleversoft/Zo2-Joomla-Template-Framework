3068332bae7b0fe403d70fdf954b74b42844132b
Dung Tran <tad88.dev@gmail.com>
2014-12-10 16:00:15 +0700

    ^hot fix

--- plugins/system/zo2/framework/html/fields/image.php
+++ plugins/system/zo2/framework/html/fields/image.php
@@ -12,6 +12,54 @@
 defined('_JEXEC') or die;
 JHtml::_('behavior.modal', 'a.modal');
 $elementId = $this->data['id'];
+/**
+ * Temporary hot fix
+ * @todo using commedia api
+ */
+$script = array();
+$script[] = '	function jInsertFieldValue(value, id) {';
+$script[] = '		var $ = jQuery.noConflict();';
+$script[] = '		var old_value = $("#" + id).val();';
+$script[] = '		if (old_value != value) {';
+$script[] = '			var $elem = $("#" + id);';
+$script[] = '			$elem.val(value);';
+$script[] = '			$elem.trigger("change");';
+$script[] = '			if (typeof($elem.get(0).onchange) === "function") {';
+$script[] = '				$elem.get(0).onchange();';
+$script[] = '			}';
+$script[] = '			jMediaRefreshPreview(id);';
+$script[] = '		}';
+$script[] = '	}';
+
+$script[] = '	function jMediaRefreshPreview(id) {';
+$script[] = '		var $ = jQuery.noConflict();';
+$script[] = '		var value = $("#" + id).val();';
+$script[] = '		var $img = $("#" + id + "_preview");';
+$script[] = '		if ($img.length) {';
+$script[] = '			if (value) {';
+$script[] = '				$img.attr("src", "' . JUri::root() . '" + value);';
+$script[] = '				$("#" + id + "_preview_empty").hide();';
+$script[] = '				$("#" + id + "_preview_img").show()';
+$script[] = '			} else { ';
+$script[] = '				$img.attr("src", "")';
+$script[] = '				$("#" + id + "_preview_empty").show();';
+$script[] = '				$("#" + id + "_preview_img").hide();';
+$script[] = '			} ';
+$script[] = '		} ';
+$script[] = '	}';
+
+$script[] = '	function jMediaRefreshPreviewTip(tip)';
+$script[] = '	{';
+$script[] = '		var $ = jQuery.noConflict();';
+$script[] = '		var $tip = $(tip);';
+$script[] = '		var $img = $tip.find("img.media-preview");';
+$script[] = '		$tip.find("div.tip").css("max-width", "none");';
+$script[] = '		var id = $img.attr("id");';
+$script[] = '		id = id.substring(0, id.length - "_preview".length);';
+$script[] = '		jMediaRefreshPreview(id);';
+$script[] = '		$tip.show();';
+$script[] = '	}';
+JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 ?>
 <div class="control-group  <?php echo (isset($this->label['class_wrap'])) ? $this->label['class_wrap'] : ''; ?>">
     <!-- Label -->
