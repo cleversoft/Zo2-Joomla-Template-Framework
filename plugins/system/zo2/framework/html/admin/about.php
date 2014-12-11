5ed71b2d27772b2e5f1f30b550a12e2b1d9528c4
phuongnd <nguyenducphuong@phuongs-iMac-2.local>
2014-12-11 11:14:50 +0700

    Fixed mistypo in "about us" tab
    + fixed mistypo in about us tab,
    + update target=“_blank”

--- plugins/system/zo2/framework/html/admin/about.php
+++ plugins/system/zo2/framework/html/admin/about.php
@@ -75,29 +75,29 @@
                     <h3>Contributing</h3>
                     <p>Everyone is welcome to help contribute and improve this project. There are several ways you can contribute:</p>
                     <ul class="task-list">
-						<li>Reporting issues (please read <a href="https://github.com/necolas/issue-guidelines">issue guidelines</a>)</li>
+						<li>Reporting issues (please read <a href="https://github.com/necolas/issue-guidelines" target="_blank">issue guidelines</a>)</li>
 						<li>Suggesting new features</li>
 						<li>Writing or refactoring code</li>
-						<li>Fixing <a href="https://github.com/cleversoft/zo2/issues">issues</a></li>
-						<li>Replying to questions on <a href="http://www.zootemplate.com/zo2-support">the forum</a></li>
+						<li>Fixing <a href="https://github.com/cleversoft/zo2/issues" target="_blank">issues</a></li>
+						<li>Replying to questions on <a href="http://www.zootemplate.com/zo2-support" target="_blank">the forum</a></li>
 					</ul>
 					
                     <h3>Documentation</h3>
                     <p><a class="btn btn-primary btn-large" href="http://docs.zootemplate.com/category/zo2" target="_blank">Online Documents</a></p>
                     <div class="zo2-divider"></div>
                     <h3>Supports</h3>
-                    <p>Use the <a href="http://www.zootemplate.com/zo2-support">Zo2 Support</a> to ask questions and get support.</p>
+                    <p>Use the <a href="http://www.zootemplate.com/zo2-support" target="_blank">Zo2 Support</a> to ask questions and get support.</p>
                     <div class="zo2-divider"></div>
                     <h2>Copyright, License &amp; Usage</h2>
-                   	<p>Copyright (c) 2008 - 2014 <a href="http://cleversoft.co">CleverSoft Solutions</a></p>
+                   	<p>Copyright (c) 2008 - 2014 <a href="http://cleversoft.co" target="_blank">CleverSoft Solutions</a></p>
                     <h3>License</h3>
-                    <p>Zo2Framework is licensed under GNU General Public License (GPL), version 2 or later. <br /> <a href="http://www.gnu.org/licenses/gpl-2.0.html">http://www.gnu.org/licenses/gpl-2.0.html</a></p>
+                    <p>Zo2Framework is licensed under GNU General Public License (GPL), version 2 or later. <br /> <a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">http://www.gnu.org/licenses/gpl-2.0.html</a></p>
                     <h3>Usage</h3>
                     <p>You are free to use Zo2 Framework for your personal and commercial projects. You do not have to keep the Zo2 or Zootemplate links and logos in the front end, however, you must ensure that all copyright notices in the code are retained. Also, if you intend to use the Zo2 Framework in a commercial project, or a template you intend to redistribute in any form, please retain a "Powered by Zo2" logo and link in the backend administrative interface.</p>
                     <h3>Credits</h3>
                     <ul class="task-list">
-						<li><a href="http://fortawesome.github.io/Font-Awesome/icons/">FontAwesome Icons</a>)</li>
-						<li><a href="http://getbootstrap.com/">BootStrap Framework</a>)</li>
+						<li><a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome Icons</a></li>
+						<li><a href="http://getbootstrap.com/" target="_blank">BootStrap Framework</a></li>
 					</ul>
                 </div>
             </div>
