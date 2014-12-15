b1b4ae47e8d78940cf28ef2a02331ed55d4ff316
Viet Vu <jooservices@gmail.com>
2014-12-15 14:46:04 +0700

    ^ temporary fix for layout builder modal

--- plugins/system/zo2/framework/html/admin/body/sidebar/builder/layout.column.php
+++ plugins/system/zo2/framework/html/admin/body/sidebar/builder/layout.column.php
@@ -17,7 +17,7 @@
         <div class="col-name"><?php echo $column->get('name'); ?></div>
         <div class="col-control-buttons">
             <i title="Drag column" class="col-control-icon dragger fa fa-arrows hasTooltip"></i>
-            <i title="Column's settings" class="fa fa-cog col-control-icon settings hasTooltip"></i>
+            <i title="Column's settings" class="fa fa-cog col-control-icon settings hasTooltip" data-toggle="modal" data-target="#colSettingsModal"></i>
             <i title="Append new row" class="col-control-icon add-row fa fa-align-justify hasTooltip"></i>
             <i title="Remove column" class="fa fa-remove col-control-icon delete hasTooltip"></i>
         </div>
