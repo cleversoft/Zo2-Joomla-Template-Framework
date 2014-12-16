7e2c94101e0346c9d16924f3fd54039ce15cc552
Dung Tran <tad88.dev@gmail.com>
2014-12-16 12:36:33 +0700

    -remove Assets.ajax hard code

--- plugins/system/zo2/framework/html/fields/megamenu.php
+++ plugins/system/zo2/framework/html/fields/megamenu.php
@@ -11,21 +11,6 @@ $vars = array();
 $calls = array();
 $attr = '';
 
-$input = JFactory::getApplication()->input;
-if ($input->getCmd('option') == 'com_templates' &&
-        (preg_match('/style\./', $input->getCmd('task')) || $input->getCmd('view') == 'style' || $input->getCmd('view') == 'template')
-) {
-    $vars['url'] = JUri::root() . 'index.php?zo2controller=menu&task=display';
-}
-$calls[] = 'Assets.ajax(\'' . $this->data['name'] . '\', ' . json_encode($vars) . ');';
-
-$html[] = '<script type="text/javascript">';
-$html[] = '         jQuery(window).on(\'load\', function(){';
-$html[] = '             ' . implode("\n", $calls) . '';
-$html[] = '         })';
-$html[] = '</script>';
-
-
 // Get the field options.
 $options = (array) JHtml::_('menu.menus');
 
