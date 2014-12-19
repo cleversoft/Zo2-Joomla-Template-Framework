c4ed9bf1f2c95f04d2bf863b5557bd520a2539cf
Viet Vu <jooservices@gmail.com>
2014-12-19 09:32:55 +0700

    - remove old clear cache

--- plugins/system/zo2/framework/controller.php
+++ plugins/system/zo2/framework/controller.php
@@ -94,19 +94,4 @@ class Zo2Controller {
         }
     }
 
-    public static function clearCache() {
-        $layoutDir = Zo2Factory::getPath('templates://layouts/cache');
-        $rootCacheDir =  Zo2Factory::getPath('cache://');
-        $files = glob($layoutDir . '/*');
-        $cacheFiles = glob($rootCacheDir . '/*');
-        foreach ($files as $file) {
-            if (is_file($file))
-                unlink($file);
-        }
-        foreach ($cacheFiles as $file) {
-            if (is_file($file))
-                unlink($file);
-        }
-    }
-
 }
