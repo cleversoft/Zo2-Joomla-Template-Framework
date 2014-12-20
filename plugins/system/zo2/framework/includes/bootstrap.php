8d5bd062d74c450859094f2ed701d71ab468f895
Viet Vu <jooservices@gmail.com>
2014-12-20 16:29:34 +0700

    ^ temporary remove download Profiles

--- plugins/system/zo2/framework/includes/bootstrap.php
+++ plugins/system/zo2/framework/includes/bootstrap.php
@@ -68,15 +68,18 @@ if (Zo2Factory::isZo2Template()) {
                         $model->rename();
                         break;
                 };
-                $zo2Task = $jinput->get('zo2_task');
-                if ($zo2Task) {
-                    $zo2Task = explode('.', $zo2Task);
-                    $modelClass = 'Zo2Model' . ucfirst($zo2Task[0]);
-                    $model = new $modelClass;
-                    if (method_exists($model, $zo2Task[1])) {
-                        call_user_func(array($model, $zo2Task[1]));
-                    }
-                }
+                /*
+                  $zo2Task = $jinput->get('zo2_task');
+                  if ($zo2Task) {
+                  $zo2Task = explode('.', $zo2Task);
+                  $modelClass = 'Zo2Model' . ucfirst($zo2Task[0]);
+                  $model = new $modelClass;
+                  if (method_exists($model, $zo2Task[1])) {
+                  call_user_func(array($model, $zo2Task[1]));
+                  }
+                  }
+                 * 
+                 */
             }
         }
     JFactory::getDocument()->addScriptDeclaration('/**
