b0886d23352689609b01c6421a08f7a0606b0ebf
Viet Vu <jooservices@gmail.com>
2014-12-25 15:25:11 +0700

    ^ missed update id field for col

--- plugins/system/zo2/framework/layout.php
+++ plugins/system/zo2/framework/layout.php
@@ -323,7 +323,7 @@ if (!class_exists('Zo2Layout')) {
 
                 /* BEGIN COL */
                 $id = JFilterOutput::stringURLSafe(strtolower(trim($item->get('name', $item->get('position')))));
-                $html .= '<div id="' . $item->get('id') . '" class="' . $customClass . $class . '">';
+                $html .= '<div id="' .$id . '" class="' . $customClass . $class . '">';
 
                 switch ($jdoc) {
                     case 'component':
