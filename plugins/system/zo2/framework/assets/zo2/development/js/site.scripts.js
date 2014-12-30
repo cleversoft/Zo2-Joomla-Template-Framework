b1a044c1684dd8b52ae206c72b1f6e8eedf27ac2
Dung Tran <tad88.dev@gmail.com>
2014-12-30 23:42:09 +0700

    # fix site.script.js

--- plugins/system/zo2/framework/assets/zo2/development/js/site.scripts.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/site.scripts.js
@@ -1,16 +1,6 @@
-(function (window, zo2, $) {
-    zo2.site = {
+(function (w, z, $) {
+    var _site = {
     };
+    /** Init zo2.site **/
+    z.site = _site;
 })(window, zo2, zo2.jQuery);
-
-jQuery(document).ready(function () {
-
-    /* Default active tab for shortcode tabs*/
-    jQuery('ul.nav-tabs.shortcode a:first').tab('show'); // Select first tab
-
-    jQuery('.zo2-megamenu .navbar-nav a').click(function () {
-        var element = jQuery(this).attr('href');
-        zo2.animate.scrollTop('html, body', element);
-    });
-
-});
