c72ecc71b87d62b5ceec5edacf25c5b6cc41340f
Viet Vu <jooservices@gmail.com>
2015-01-01 10:26:56 +0700

    ^ update custom code language & render

--- plugins/system/zo2/framework/html/site/layout/default.php
+++ plugins/system/zo2/framework/html/site/layout/default.php
@@ -21,7 +21,8 @@ $bodyClasses = trim(implode(' ', $bodyClasses));
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $framework->document->getLanguage(); ?>" lang="<?php echo $framework->document->getLanguage(); ?>" dir="<?php echo $framework->document->getDirection(); ?>">
-    <head>              
+    <head>       
+        <?php echo Zo2Framework::getProfileParam('custom_code_after_open_head'); ?>
         <jdoc:include type="head" />        
         <!-- built with zo2 framework: http://www.zootemplate.com/zo2 -->
         <?php require_once Zo2Path::getInstance()->getPath('Zo2://assets/js/zo2.php'); ?>
@@ -29,6 +30,7 @@ $bodyClasses = trim(implode(' ', $bodyClasses));
         <?php if ($framework->profile->get('enable_responsive', true)) : ?>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <?php endif; ?>
+        <?php echo Zo2Framework::getProfileParam('custom_code_before_close_head'); ?>
     </head>
     <body id="zo2-framework-wrapper" class="<?php echo $bodyClasses; ?>"
           j-option="<?php echo JFactory::getApplication()->input->getCmd('option'); ?>"
@@ -37,6 +39,8 @@ $bodyClasses = trim(implode(' ', $bodyClasses));
           zo2-template="<?php echo $framework->template->get('template'); ?>"
           zo2-profile="<?php echo $framework->profile->get('name'); ?>"
           >       
+              <?php echo Zo2Framework::getProfileParam('custom_code_after_open_body'); ?>
               <?php echo $layout->render(); ?>
+              <?php echo Zo2Framework::getProfileParam('custom_code_before_close_body'); ?>
     </body>
 </html>
