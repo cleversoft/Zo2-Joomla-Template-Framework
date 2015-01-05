ba087495dc1e05b1c0d8bd5d0acfd262f51a7a13
Dung Tran <tad88.dev@gmail.com>
2015-01-05 22:26:46 +0700

    + add empty layout builder

---
+++ plugins/system/zo2/framework/assets/js/layoutbuilder.js
@@ -0,0 +1,17 @@
+/**
+ * Zo2 Layout builder
+ * @param {type} w
+ * @param {type} z
+ * @param {type} $
+ * @returns {undefined}
+ */
+(function(w, z, $){
+    
+    /* Layout builder local */
+    var _layoutbuilder = {
+        
+    };
+    
+    zo2.layoutbuilder = _layoutbuilder;
+    
+})(window, zo2, jQuery);
