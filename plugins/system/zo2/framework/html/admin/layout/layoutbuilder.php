9753c3cd66a2e23b78fa8b701adce5681330b5e1
Viet Vu <jooservices@gmail.com>
2015-01-12 11:33:09 +0700

    ^ add empty row into parent of layout builder

--- plugins/system/zo2/framework/html/admin/layout/layoutbuilder.php
+++ plugins/system/zo2/framework/html/admin/layout/layoutbuilder.php
@@ -1,13 +1,21 @@
 <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
 <div id="layoutbuilder-container" class="row">
+    <div class="empty-row">
+        <?php
+        $blankRow = new Zo2LayoutbuilderRow();
+        $this->set('child', false);
+        $this->set('row', $blankRow);
+        $this->load('Zo2://html/admin/layout/layoutbuilder.row.php');
+        ?>
+    </div>
     <div id="zo2-layoutbuilder-controls">
         <button onclick="zo2.layoutbuilder.addParentRow();" type="button" class="btn btn-primary">New row</button>
     </div>
     <div id="zo2-layoutbuilder">
-    <?php foreach ($layout as $row) : ?>
-        <?php $this->set('child', false); ?>
-        <?php $this->set('row', new Zo2LayoutbuilderRow($row)); ?>
-        <?php $this->load('Zo2://html/admin/layout/layoutbuilder.row.php'); ?>
-    <?php endforeach; ?>
+        <?php foreach ($layout as $row) : ?>
+            <?php $this->set('child', false); ?>
+            <?php $this->set('row', new Zo2LayoutbuilderRow($row)); ?>
+            <?php $this->load('Zo2://html/admin/layout/layoutbuilder.row.php'); ?>
+        <?php endforeach; ?>
     </div>
 </div>
