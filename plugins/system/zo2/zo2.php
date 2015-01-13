bc0dbbe0bac85bb4d8c7ddb28736fe55b6049a28
Viet Vu <jooservices@gmail.com>
2015-01-13 20:23:18 +0700

    - remove unused google authorship

--- plugins/system/zo2/zo2.php
+++ plugins/system/zo2/zo2.php
@@ -16,17 +16,20 @@ defined('_JEXEC') or die('Restricted access');
 /**
  * Class exists checking
  */
-if (!class_exists('plgSystemZo2')) {
+if (!class_exists('plgSystemZo2'))
+{
 
     /**
      * Zo2 Framework entrypoint plugin
      */
-    class plgSystemZo2 extends JPlugin {
+    class plgSystemZo2 extends JPlugin
+    {
 
         /**
          * Init our framework
          */
-        public function onAfterRoute() {
+        public function onAfterRoute()
+        {
             include_once __DIR__ . '/framework/includes/bootstrap.php';
             Zo2Factory::ajax();
         }
@@ -34,8 +37,10 @@ if (!class_exists('plgSystemZo2')) {
         /**
          * 
          */
-        public function onAfterRender() {
-            if (Zo2Factory::isZo2Template()) {
+        public function onAfterRender()
+        {
+            if (Zo2Factory::isZo2Template())
+            {
                 $app = JFactory::getApplication();
                 $body = JResponse::getBody();
 
@@ -48,34 +53,6 @@ if (!class_exists('plgSystemZo2')) {
 
                 /* Apply back to body */
                 JResponse::setBody($body);
-            }
-        }
-
-        public function onContentPrepare($context, &$article, &$params, $page = 0) {
-            if (Zo2Factory::isZo2Template()) {
-                $framework = Zo2Factory::getFramework();
-                $config = Zo2Factory::getTemplate()->params;
-                // Don't run this plugin when the content is being indexed
-                if ($context == 'com_finder.indexer') {
-                    return true;
-                }
-
-                if (JFactory::getApplication()->isSite()) {
-                    //$article->text = $this->doShortCode($article->text);
-                    /* Google Authorship */
-                    if ((int) $config->get("enable_googleauthorship", 0)) {
-                        $author_name = "+";
-                        $user = JFactory::getUser();
-                        $rel = 'me';
-                        if ($user->get('id') == $article->created_by) {
-                            $rel = 'author';
-                        }
-
-                        $gplus = '<a href="' . $config->get('google_profile_url', '') . '/?rel=' . $rel . '"';
-                        $gplus .= ' title="Google Plus Profile for ' . $author_name . '" plugin="Google Plus Authorship">' . $author_name . '</a>';
-                        $article->text = $gplus . $article->text;
-                    }
-                }
             }
         }
 
