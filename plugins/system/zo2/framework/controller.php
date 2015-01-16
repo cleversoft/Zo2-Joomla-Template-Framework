89a8f7e5bd007070fe5a1621ef841e1dea150d5c
Viet Vu <jooservices@gmail.com>
2015-01-16 09:59:03 +0700

    ^ code clean up

--- plugins/system/zo2/framework/controller.php
+++ plugins/system/zo2/framework/controller.php
@@ -13,31 +13,38 @@
 //no direct accees
 defined('_JEXEC') or die('resticted aceess');
 
-class Zo2Controller {
+class Zo2Controller
+{
 
-    public static function exec($controller) {
-        if (method_exists('Zo2Controller', $controller)) {
+    public static function exec($controller)
+    {
+        if (method_exists('Zo2Controller', $controller))
+        {
             Zo2Controller::$controller();
         }
         exit;
     }
 
-    public static function menu() {
+    public static function menu()
+    {
         $task = JFactory::getApplication()->input->get('task', '');
         Zo2Factory::import('core.classes.admin.menu');
-        if (method_exists('AdminMenu', $task)) {
+        if (method_exists('AdminMenu', $task))
+        {
             echo AdminMenu::$task();
             exit;
         }
     }
 
-    public static function module() {
-
-     
+    public static function module()
+    {
+        
     }
 
-    public static function saveLayout() {
-        if ($_POST && isset($_POST['name']) && isset($_POST['data']) && isset($_POST['template'])) {
+    public static function saveLayout()
+    {
+        if ($_POST && isset($_POST['name']) && isset($_POST['data']) && isset($_POST['template']))
+        {
             $templatePath = JPATH_SITE . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $_POST['template'] . DIRECTORY_SEPARATOR .
                     'layouts' . DIRECTORY_SEPARATOR . $_POST['name'] . '.json';
 
@@ -48,30 +55,38 @@ class Zo2Controller {
     /**
      * 
      */
-    public static function getLayout() {
-        if (isset($_GET['layout']) && $_GET['template']) {
+    public static function getLayout()
+    {
+        if (isset($_GET['layout']) && $_GET['template'])
+        {
             $layout = Zo2Factory::getFramework()->getLayout($_GET['template']);
             echo $layout->getLayoutJson(true, true);
         }
     }
 
-    public static function getComponents() {
-        if ($_GET['template']) {
+    public static function getComponents()
+    {
+        if ($_GET['template'])
+        {
             header('Content-Type: application/json');
             echo Zo2Factory::getFramework()->getComponents($_GET['template']);
         }
     }
 
-    public static function getLayouts() {
-        if ($_GET['template']) {
+    public static function getLayouts()
+    {
+        if ($_GET['template'])
+        {
             header('Content-Type: application/json');
             echo Zo2Factory::getFramework()->getTemplateLayoutsName($_GET['template']);
         }
     }
 
-    public static function getFonts() {
+    public static function getFonts()
+    {
         header('Content-Type: application/json');
-        if (isset($_GET['query'])) {
+        if (isset($_GET['query']))
+        {
             echo json_encode(Zo2HelperGoogleFonts::search($_GET['query']));
         }
     }
