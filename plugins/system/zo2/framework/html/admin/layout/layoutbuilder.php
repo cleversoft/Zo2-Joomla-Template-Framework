f77f3668386bc258d9bcc21a82154127059c6bc3
Dung Tran <tad88.dev@gmail.com>
2015-01-20 14:37:32 +0700

    + add delete modal confirm

--- plugins/system/zo2/framework/html/admin/layout/layoutbuilder.php
+++ plugins/system/zo2/framework/html/admin/layout/layoutbuilder.php
@@ -18,6 +18,23 @@
         ?>
     </div>
     <?php $this->load('Zo2://html/admin/layout/layoutbuilder.settings.php'); ?>
+    <div class="modal fade" id="zo2-delete-confirm">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><?php echo(JText::_('ZO2_ADMIN_LAYOUTBUILDER_MODAL_DELETE_CONFIRM_TITLE')); ?></h4>
+                </div>
+                <div class="modal-body">
+                    <p><?php echo(JText::_('ZO2_ADMIN_LAYOUTBUILDER_MODAL_DELETE_CONFIRM_CONTENT')); ?></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" onclick="zo2.layoutbuilder.rowDelete();"><?php echo(JText::_('ZO2_ADMIN_LAYOUTBUILDER_MODAL_DELETE_CONFIRM_CONFIRM')); ?></button>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal"><?php echo(JText::_('ZO2_ADMIN_LAYOUTBUILDER_MODAL_DELETE_CONFIRM_CANCEL')); ?></button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="zo2-layoutbuilder-controls">
         <button onclick="zo2.layoutbuilder.addParentRow();" type="button" class="btn btn-primary">New row</button>
     </div>
