75ec5dad62c4bec40dfa4ccfdd4333b50ca7f9fb
Viet Vu <jooservices@gmail.com>
2015-01-20 09:52:36 +0700

    ^ replace Joomla! BS2 js by BS3 js

--- plugins/system/zo2/framework/depends/3.3/admin.php
+++ plugins/system/zo2/framework/depends/3.3/admin.php
@@ -3,6 +3,22 @@
 Zo2Framework::log('Load depends: ' . __FILE__);
 JHtml::_('jquery.framework');
 JHtml::_('jquery.ui', array('core', 'sortable'));
+JHtml::_('bootstrap.framework');
+
+$doc = JFactory::getDocument();
+
+$replacedScripts = array();
+foreach ($doc->_scripts as $key => $value)
+{
+    if ($key == JUri::root(true) . '/media/jui/js/bootstrap.min.js')
+    {
+        $replacedScripts[JUri::root(true) . '/plugins/system/zo2/framework/assets/vendor/bootstrap/js/bootstrap.min.js'] = $value;
+    } else
+    {
+        $replacedScripts[$key] = $value;
+    }
+}
+$doc->_scripts = $replacedScripts;
 
 $zo2Assets = Zo2Path::getInstance()->getPath('Zo2://assets/admin.json');
 
