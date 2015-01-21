c104320464efb58f60ab616b0f203d26ce361363
chinhbeo <chinhlv91@gmail.com>
2015-01-21 18:37:46 +0700

    + Add Description for row custom classs Layout Builder

--- plugins/system/zo2/framework/html/admin/body/sidebar/builder/layout.php
+++ plugins/system/zo2/framework/html/admin/body/sidebar/builder/layout.php
@@ -62,7 +62,7 @@ $positions = $model->getAvaiablePositions();
                             </div>
                         </div>
                         <div class="control-group">
-                            <label class="control-label" for="txtRowCss"><?php echo JText::_('ZO2_ADMIN_FORMFIELD_LAYOUT_CUSTOM_CSS'); ?></label>
+                            <label class="control-label" for="txtRowCss"><?php echo JText::_('ZO2_ADMIN_FORMFIELD_LAYOUT_CUSTOM_CSS'); ?><small><br /><?php echo JText::_('ZO2_ADMIN_LAYOUT_BUILDER_NOTE_CUSTOM_CLASS'); ?></small></label>
                             <div class="controls">
                                 <input type="text" id="txtRowCss" placeholder="<?php echo JText::_('ZO2_ADMIN_FORMFIELD_LAYOUT_CUSTOM_ROWCSS'); ?>">
                             </div>
