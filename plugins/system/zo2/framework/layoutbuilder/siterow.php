1f491dfeb4bd4c2ab24a6e235b7a7bf31dbb45c0
Viet Vu <jooservices@gmail.com>
2015-01-30 14:00:19 +0700

    + add missed file

---
+++ plugins/system/zo2/framework/layoutbuilder/siterow.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Zo2 (http://www.zootemplate.com/zo2)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zootemplate.com/zo2
+ * @link        https://github.com/cleversoft/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2014 CleverSoft (http://cleversoft.co/)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+
+if (!class_exists('Zo2LayoutbuilderSiterow'))
+{
+
+    /**
+     * Class object for each row
+     */
+    class Zo2LayoutbuilderSiterow extends Zo2LayoutbuilderAbstract
+    {
+
+        /**
+         *
+         * @param type $default
+         * @return string
+         */
+        public function getClass($default = array())
+        {
+            $class = $default;
+            $class [] = '';
+            if ($this->get('class') != '')
+                $class [] = $this->get('class');
+            // Only generate grid class for children
+            if (!$this->isRoot())
+            {
+                $class [] = $this->_getGridClass();
+            } else
+            {
+                // Add more class for root
+            }
+
+            return implode(' ', $class);
+        }
+
+    }
+
+}
