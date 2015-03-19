fbd86f9a6c2d076ece5dc7ae555d9fa26d7f68cb
Dung Tran <tad88.dev@gmail.com>
2015-03-19 13:56:15 +0700

    # Fix conflict after update 3.4

---
+++ plugins/system/zo2/framework/depends/3.4/site.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Zo2 (http://www.zootemplate.com/zo2)
+ * A powerful Joomla template framework
+ *
+ * @link        http://www.zootemplate.com/zo2
+ * @link        https://github.com/cleversoft/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2014 CleverSoft (http://cleversoft.co/)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+
+Zo2Framework::log('Load depends: ' . __FILE__);
+// Load jQuery Framework core
+JHtml::_('jquery.framework');
+
+// Init assets
+$zo2Assets = Zo2Path::getInstance()->getPath('Zo2://assets/site.json');
+if ($zo2Assets)
+{
+    $assets = Zo2Assets::getInstance();
+    $zoAssets = json_decode(file_get_contents($zo2Assets));
+    $assets->load($zoAssets);
+}
