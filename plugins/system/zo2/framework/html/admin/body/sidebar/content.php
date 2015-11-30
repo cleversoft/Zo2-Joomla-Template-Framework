5f310fe5a4c93965c58bbce900e489387311adaa
Viet Vu <viet@redweb.dk>
2015-11-30 16:28:19 +0700

    Remove Fonts settings

--- plugins/system/zo2/framework/html/admin/body/sidebar/content.php
+++ plugins/system/zo2/framework/html/admin/body/sidebar/content.php
@@ -1,6 +1,5 @@
 <div id="myTabZo2SidebarContent" class="tab-content">
     <?php $this->load('admin/body/sidebar/content_general.php'); ?>
-    <?php $this->load('admin/body/sidebar/content_fonts.php'); ?>
     <?php $this->load('admin/body/sidebar/content_layout.php'); ?>
     <?php $this->load('admin/body/sidebar/content_themecolor.php'); ?>
     <?php $this->load('admin/body/sidebar/content_menu.php'); ?>    
