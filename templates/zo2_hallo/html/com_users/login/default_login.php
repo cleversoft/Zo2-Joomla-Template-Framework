c719995223f0fa2beaa0784e85abfab47ef03051
Hanh Phan <phanngochanh.pnh@gmail.com>
2016-03-29 10:10:26 +0700

    override layout login com_user

--- templates/zo2_hallo/html/com_users/login/default_login.php
+++ templates/zo2_hallo/html/com_users/login/default_login.php
@@ -52,7 +52,7 @@ JHtml::_('behavior.keepalive');
 			<?php endforeach; ?>
 			<div class="control-group">
 				<div class="controls">
-					<button type="submit" class="btn btn-primary"><?php echo JText::_('JLOGIN'); ?></button>
+					<button type="submit" class="btn btn-success"><?php echo JText::_('JLOGIN'); ?></button>
 				</div>
 			</div>
 			<input type="hidden" name="return" value="<?php echo base64_encode($this->params->get('login_redirect_url', $this->form->getValue('return'))); ?>" />
