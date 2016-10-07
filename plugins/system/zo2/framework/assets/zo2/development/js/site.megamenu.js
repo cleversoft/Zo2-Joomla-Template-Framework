d963586ac097caae5868b1b6b21fe2db940bdaf5
thanhppm <thanhppm@gmail.com>
2016-10-07 16:22:08 +0700

    Add inner canvas menu in megamenu

--- plugins/system/zo2/framework/assets/zo2/development/js/site.megamenu.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/site.megamenu.js
@@ -152,5 +152,29 @@
             else $this.removeClass('icon-caret-down').addClass('icon-caret-up');
         });
         /** END: off canvas menu **/
+
+        var $body = $('body');
+        $body.append('<div id="nm-page-overlay" class="nm-page-overlay"></div>');
+        $('#nm-mobile-menu-button').click(function(){
+            if($body.hasClass('mobile-menu-open')){
+                $body.removeClass('mobile-menu-open');
+                $('#nm-page-overlay').removeClass('show');
+                $('.navbar-collapse').addClass('collapse');
+            }else{
+                $body.addClass('mobile-menu-open');
+                $('#nm-page-overlay').addClass('show');
+                $('#zo2-mega-menu nav.zo2-menu').attr('id','mn-menu-canvas');
+                $('#nm-page-overlay').addClass('show');
+                $('.navbar-collapse').removeClass('collapse').addClass('nm-mobile-menu-content');            }
+
+            $('.mobile-menu-open #nm-page-overlay').click(function(){
+                $body.removeClass('mobile-menu-open');
+                $(this).removeClass('show');
+                $('.navbar-collapse').addClass('collapse');
+            })
+
+        })
+
+        
     });
 }(jQuery);
