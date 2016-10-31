0106ea998778897bfb712d86338fda55819e1a48
Thuan1234 <codejoomla@gmail.com>
2016-10-31 10:36:38 +0700

    - fixed #308

--- plugins/system/zo2/framework/assets/zo2/development/js/admin.layoutbuilder.js
+++ plugins/system/zo2/framework/assets/zo2/development/js/admin.layoutbuilder.js
@@ -47,7 +47,7 @@
             allColClass: 'col-md-1 col-md-2 col-md-3 col-md-4 col-md-5 col-md-6 col-md-7 col-md-8 col-md-9 col-md-10 col-md-11 col-md-12',
             /* Colum offset */
             allColOffset: 'col-md-offset-0 col-md-offset-1 col-md-offset-2 col-md-offset-3 col-md-offset-4 col-md-offset-5 col-md-offset-6 ' +
-                    'col-md-offset-7 col-md-offset-8 col-md-offset-9 col-md-offset-10 col-md-offset-11 col-md-offset-12'
+            'col-md-offset-7 col-md-offset-8 col-md-offset-9 col-md-offset-10 col-md-offset-11 col-md-offset-12'
         },
         /**
          * Element which is editing
@@ -69,7 +69,7 @@
         },
 
         /*
-        prepend column
+         prepend column
          */
 
         addNewBlock: function() {
@@ -103,7 +103,7 @@
         },
 
         /*
-        setting new column
+         setting new column
          */
         /**
          * Init sortable for layout builder
@@ -219,7 +219,7 @@
          */
 
         /*
-        * clone row
+         * clone row
          */
         cloneRow: function($target) {
             var clone = $target.clone().wrap('<div>').parent().html();
@@ -252,18 +252,18 @@
             $col.attr('data-zo2-offset', 0);
             $col.attr('data-zo2-customClass', '');
             var metaHtml = '<div class="col-control-header">'+
-                    '<i title="'+prepend_this_column+'" class="row-control-icon prepend-column before fa fa-plus hasTooltip"></i>'+
-                    '<i title="'+col_setting+'" class="fa fa-cog col-control-icon settings hasTooltip" data-toggle="modal"></i>'+
-                    '<i title="'+remove_col+'" class="row-control-icon delete fa fa-trash-o hasTooltip"></i>'+
-                    '</div><div class="col-wrap sortable-col-child col-md-12" data-zo2-type="span" data-zo2-position ="" data-zo2-offset="" data-zo2-customClass =""> <div class="col-name">(none)</div>' +
-                    '<div class="col-control-buttons">' +
-                    '<i title="'+drag_block+'" class="col-control-icon dragger fa fa-arrows hasTooltip"></i>' +
-                    '<i title="'+clone_block+'" class="col-control-icon add-row fa fa-clone hasTooltip"></i>'+
-                    '<i title="'+block_setting+'" class="fa fa-cog col-control-icon block-settings hasTooltip" data-toggle="modal"></i>'+
-                    '<i title="'+remove_block+'" class="fa fa-remove col-control-icon delete hasTooltip"></i>' +
-                    '</div><div class="row-container"></div>' +
-                    '</div>'+
-                    '<div class="col-prepend-after"><i title="'+prepend_this_column+'" class="row-control-icon prepend-column after fa fa-plus hasTooltip"></i></div></div>';
+                '<i title="'+prepend_this_column+'" class="row-control-icon prepend-column before fa fa-plus hasTooltip"></i>'+
+                '<i title="'+col_setting+'" class="fa fa-cog col-control-icon settings hasTooltip" data-toggle="modal"></i>'+
+                '<i title="'+remove_col+'" class="row-control-icon delete fa fa-trash-o hasTooltip"></i>'+
+                '</div><div class="col-wrap sortable-col-child col-md-12" data-zo2-type="span" data-zo2-position ="" data-zo2-offset="" data-zo2-customClass =""> <div class="col-name">(none)</div>' +
+                '<div class="col-control-buttons">' +
+                '<i title="'+drag_block+'" class="col-control-icon dragger fa fa-arrows hasTooltip"></i>' +
+                '<i title="'+clone_block+'" class="col-control-icon add-row fa fa-clone hasTooltip"></i>'+
+                '<i title="'+block_setting+'" class="fa fa-cog col-control-icon block-settings hasTooltip" data-toggle="modal"></i>'+
+                '<i title="'+remove_block+'" class="fa fa-remove col-control-icon delete hasTooltip"></i>' +
+                '</div><div class="row-container"></div>' +
+                '</div>'+
+                '<div class="col-prepend-after"><i title="'+prepend_this_column+'" class="row-control-icon prepend-column after fa fa-plus hasTooltip"></i></div></div>';
             var $meta = $(metaHtml);
             $meta.appendTo($col);
             $col.appendTo($target);
@@ -276,33 +276,33 @@
          * @returns {undefined}
          */
         _addChildRow: function($target, after) {
-                var _self = this;
-                if(typeof(after) === 'undefined'){
-                    var $row = $('<div />').addClass('zo2-row sortable-row').appendTo($target);
-                }else{
-                    if (after == 'prependTo') var $row = $('<div />').addClass('zo2-row sortable-row').insertBefore($target);
-                    else var $row = $('<div />').addClass('zo2-row sortable-row').insertAfter($target);
-                }
-                $row.attr('data-zo2-type', 'row');
-                $row.attr('data-zo2-customClass', '');
-                $row.attr('data-zo2-fluidwidth', '0');
-                for (var i = 0; i < _self._settings.visibilityAttributes.length; i++) {
-                    $row.attr(_self._settings.visibilityAttributes[i], '1');
-                }
-                var $meta = $('<div class="col-md-12 row-control">' +
-                        '<div class="row-control-container">' +
-                        '<div class="row-control-buttons row-name">' +
-                        '<i title="'+drag_row+'" class="fa fa-arrows row-control-icon dragger hasTooltip"></i>' +
-                        '<i title="'+add_nc+'" class="row-control-icon add-column fa fa-plus hasTooltip"></i>' +
-                    'Section Title</div>' +
-                        '<div class="row-control-buttons">' +
-                        '<i title="'+row_setting+'" class="fa fa-cog row-control-icon settings hasTooltip"></i>' +
-                        '<i title="'+clone_this_row+'" class="row-control-icon add-row fa fa-clone hasTooltip"></i>' +
-                        '<i title="'+remove_row+'" class="row-control-icon delete fa fa-remove hasTooltip"></i>' +
-                        '</div></div>' +
-                        '<div class="col-container"></div></div>');
-                $meta.appendTo($row);
-                _self._addChildColumn($row.find('>.col-md-12>.col-container'))
+            var _self = this;
+            if(typeof(after) === 'undefined'){
+                var $row = $('<div />').addClass('zo2-row sortable-row').appendTo($target);
+            }else{
+                if (after == 'prependTo') var $row = $('<div />').addClass('zo2-row sortable-row').insertBefore($target);
+                else var $row = $('<div />').addClass('zo2-row sortable-row').insertAfter($target);
+            }
+            $row.attr('data-zo2-type', 'row');
+            $row.attr('data-zo2-customClass', '');
+            $row.attr('data-zo2-fluidwidth', '0');
+            for (var i = 0; i < _self._settings.visibilityAttributes.length; i++) {
+                $row.attr(_self._settings.visibilityAttributes[i], '1');
+            }
+            var $meta = $('<div class="col-md-12 row-control">' +
+            '<div class="row-control-container">' +
+            '<div class="row-control-buttons row-name">' +
+            '<i title="'+drag_row+'" class="fa fa-arrows row-control-icon dragger hasTooltip"></i>' +
+            '<i title="'+add_nc+'" class="row-control-icon add-column fa fa-plus hasTooltip"></i>' +
+            'Section Title</div>' +
+            '<div class="row-control-buttons">' +
+            '<i title="'+row_setting+'" class="fa fa-cog row-control-icon settings hasTooltip"></i>' +
+            '<i title="'+clone_this_row+'" class="row-control-icon add-row fa fa-clone hasTooltip"></i>' +
+            '<i title="'+remove_row+'" class="row-control-icon delete fa fa-remove hasTooltip"></i>' +
+            '</div></div>' +
+            '<div class="col-container"></div></div>');
+            $meta.appendTo($row);
+            _self._addChildColumn($row.find('>.col-md-12>.col-container'))
         },
         /**
          * Delet child element
@@ -347,7 +347,7 @@
         },
 
         /*
-        * block setting
+         * block setting
          */
         blocksetting: function() {
             var _self = this;
@@ -402,26 +402,22 @@
 
                 //$('#cbRowPhoneVisibility').attr('checked', $row.attr('data-zo2-visibility-xs') == '1');
                 // $('#btgRowPhone').find('button').removeClass('active btn-success btn-danger');
-                if ($row.attr('data-zo2-visibility-xs') == '1')
-                    $('#btgRowPhone').prop( "checked", true );
+                $('#btgRowPhone').prop( "checked", $row.attr('data-zo2-visibility-xs') == '1' );
                 // else
                 //     $('#btgRowPhone').find('.btn-off').addClass('active btn-danger');
                 //$('#cbRowTabletVisibility').attr('checked', $row.attr('data-zo2-visibility-sm') == '1');
                 // $('#btgRowTablet').find('button').removeClass('active btn-success btn-danger');
-                if ($row.attr('data-zo2-visibility-sm') == '1')
-                    $('#btgRowTablet').prop( "checked", true );
+                $('#btgRowTablet').prop( "checked", $row.attr('data-zo2-visibility-sm') == '1' );
                 // else
                 //     $('#btgRowTablet').find('.btn-off').addClass('active btn-danger');
                 // //$('#cbRowDesktopVisibility').attr('checked', $row.attr('data-zo2-visibility-md') == '1');
                 // $('#btgRowDesktop').find('button').removeClass('active btn-success btn-danger');
-                if ($row.attr('data-zo2-visibility-md') == '1')
-                    $('#btgRowDesktop').prop( "checked", true );
+                $('#btgRowDesktop').prop( "checked", $row.attr('data-zo2-visibility-md') == '1' );
                 // else
                 //     $('#btgRowDesktop').find('.btn-off').addClass('active btn-danger');
                 // //$('#cbRowLargeDesktopVisibility').attr('checked', $row.attr('data-zo2-visibility-lg') == '1');
                 // $('#btgRowLargeDesktop').find('button').removeClass('active btn-success btn-danger');
-                if ($row.attr('data-zo2-visibility-lg') == '1')
-                    $('#btgRowLargeDesktop').prop( "checked", true );
+                $('#btgRowLargeDesktop').prop( "checked", $row.attr('data-zo2-visibility-lg') == '1' );
                 // else
                 //     $('#btgRowLargeDesktop').find('.btn-off').addClass('active btn-danger');
 
@@ -455,26 +451,22 @@
 
                 //$('#cbColumnPhoneVisibility').attr('checked', $col.attr('data-zo2-visibility-xs') == '1');
                 // $('#btgColPhone').find('button').removeClass('active btn-danger btn-success');
-                if ($col.attr('data-zo2-visibility-xs') == '1')
-                    $('#btgColPhone').prop( "checked", true );
+                $('#btgColPhone').prop( "checked", $col.attr('data-zo2-visibility-xs') == '1' );
                 // else
                 //     $('#btgColPhone').find('.btn-off').addClass('btn-danger active');
                 // //$('#cbColumnTabletVisibility').attr('checked', $col.attr('data-zo2-visibility-sm') == '1');
                 // $('#btgColTablet').find('button').removeClass('active btn-danger btn-success');
-                if ($col.attr('data-zo2-visibility-sm') == '1')
-                    $('#btgColTablet').prop( "checked", true );
+                $('#btgColTablet').prop( "checked", $col.attr('data-zo2-visibility-sm') == '1' );
                 // else
                 //     $('#btgColTablet').find('.btn-off').addClass('btn-danger active');
                 // //$('#cbColumnDesktopVisibility').attr('checked', $col.attr('data-zo2-visibility-md') == '1');
                 // $('#btgColDesktop').find('button').removeClass('active btn-danger btn-success');
-                if ($col.attr('data-zo2-visibility-md') == '1')
-                    $('#btgColDesktop').prop( "checked", true );
+                $('#btgColDesktop').prop( "checked", $col.attr('data-zo2-visibility-md') == '1' );
                 // else
                 //     $('#btgColDesktop').find('.btn-off').addClass('btn-danger active');
                 // //$('#cbColumnLargeDesktopVisibility').attr('checked', $col.attr('data-zo2-visibility-lg') == '1');
                 // $('#btgColLargeDesktop').find('button').removeClass('active btn-danger btn-success');
-                if ($col.attr('data-zo2-visibility-lg') == '1')
-                    $('#btgColLargeDesktop').prop( "checked", true );
+                $('#btgColLargeDesktop').prop( "checked", $col.attr('data-zo2-visibility-lg') == '1' );
                 // else
                 //     $('#btgColLargeDesktop').find('.btn-off').addClass('btn-danger active');
 
@@ -489,7 +481,7 @@
                 $modal.modal('show');
             });
         },
-        /** 
+        /**
          * Save layout
          * @returns {undefined}
          */
@@ -533,7 +525,7 @@
             $('#btnSaveRowSettings').on('click', function() {
                 var $row = _self.editingElement;
                 $row.find('>.row-control>.row-control-container>.row-name').html('<i title="'+drag_row+'" class="fa fa-arrows row-control-icon dragger hasTooltip"></i>' +
-                    '<i title="'+add_nc+'" class="row-control-icon add-column fa fa-plus hasTooltip"></i>' + $('#txtRowName').val());
+                '<i title="'+add_nc+'" class="row-control-icon add-column fa fa-plus hasTooltip"></i>' + $('#txtRowName').val());
                 $row.attr('data-zo2-customClass', $('#txtRowCss').val());
                 $row.attr('data-zo2-visibility-xs', $('#btgRowPhone').is(":checked") ? '1' : '0');
                 $row.attr('data-zo2-visibility-sm', $('#btgRowTablet').is(":checked") ? '1' : '0');
