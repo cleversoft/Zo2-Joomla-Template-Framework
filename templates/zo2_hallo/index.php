2c2218d913ae38c07939030549856bc27f90e6d0
thanhppm <thanhppm@gmail.com>
2016-11-08 17:19:10 +0700

    Update zt hallo template

--- templates/zo2_hallo/index.php
+++ templates/zo2_hallo/index.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('Restricted Access');
 
 require_once __DIR__ . '/includes/bootstrap.php';
 $frontpage = '';
-$menu = & JSite::getMenu();
+$menu = &JSite::getMenu();
 if ($menu->getActive() == $menu->getDefault()) {
 $frontpage="homedefault";
 }
@@ -36,15 +36,15 @@ if ($menu->getActive()->home) {
         <!-- Enable responsive -->
         <?php if (!$this->zo2->framework->get('non_responsive_layout')) : ?>
             <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php endif; ?> 
+        <?php endif; ?>
     <jdoc:include type="head" />
 </head>
 <body class="<?php echo $frontpage.' '.$this->zo2->layout->getBodyClass(); ?> <?php echo $this->zo2->template->getDirection(); ?> <?php echo $this->zo2->framework->isBoxed() ? 'boxed' : ''; ?>">
-    <?php echo $this->zo2->template->fetch('html://layouts/css.condition.php'); ?>        
+    <?php echo $this->zo2->template->fetch('html://layouts/css.condition.php'); ?>
     <!-- Main wrapper -->
-    <section class="zo2-wrapper<?php echo $this->zo2->framework->isBoxed() ? ' boxed container' : ''; ?>">        
-        <?php echo $this->zo2->layout->render(); ?>               
-    </section>    
+    <section class="zo2-wrapper<?php echo $this->zo2->framework->isBoxed() ? ' boxed container' : ''; ?>">
+        <?php echo $this->zo2->layout->render(); ?>
+    </section>
     <?php echo $this->zo2->template->fetch('html://layouts/joomla.debug.php'); ?>
     <script type="text/javascript">
 		<?php echo $this->zo2->utilities->bottomscript->render(); ?>
